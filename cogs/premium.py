5143cde0e2300d50f536f596f5d4d5c91d91dc1c
GamingGeek <thegaminggeek362@gmail.com>
2019-05-30 19:01:28 +0100

    Added auto-role (First premium feature)

--- cogs/premium.py
+++ cogs/premium.py
@@ -39,6 +39,19 @@ class Premium(commands.Cog, name="Premium Commands"):
 		else:
 			return False
 
+	async def member_guild_check(self, member: discord.Member):
+		"""
+		Check if the guild from a member is premium
+		"""
+		if await self.bot.is_owner(member):
+			return True
+		await self.bot.db.execute(f'SELECT * FROM prefixes WHERE gid = {member.guild.id};')
+		premium = await self.bot.db.fetchone()
+		if premium != None:
+			return True
+		else:
+			return False
+
 	def gencrabrave(self, t, filename):
 		clip = VideoFileClip("crabtemplate.mp4")
 		text = TextClip(t[0], fontsize=48, color='white', font='Verdana')
@@ -76,7 +89,30 @@ class Premium(commands.Cog, name="Premium Commands"):
 		await ctx.send(file=meme)
 		os.remove(filename)
 
+	@commands.command(name='autorole', description='Automatically add a role to a user when they join')
+	async def autorole(self, ctx, role: discord.Role = None):
+		'''PFXautorole [<role name/id/mention>]\nUse command without role argument to disable'''
+		if not role:
+			await self.bot.db.execute(f'UPDATE settings SET autorole = 0 WHERE gid = {ctx.guild.id}')
+			await self.bot.conn.commit()
+			return await ctx.send(f'Successfully disabled auto-role in {ctx.guild.name}', delete_after=5)
+		else:
+			roleid = role.id
+			await self.bot.db.execute(f'UPDATE settings SET autorole = {role.id} WHERE gid = {ctx.guild.id}')
+			await self.bot.conn.commit()
+			return await ctx.send(f'Successfully enabled auto-role in {ctx.guild.name}! All new members will recieve the {role.name} role.', delete_after=5)
 
+	@commands.Cog.listener()
+	async def on_member_join(self, member):
+		if await self.member_guild_check(member):
+			await self.bot.db.execute(f'SELECT autorole FROM settings WHERE gid = {member.guild.id};')
+			role = await self.bot.db.fetchone()
+			try:
+				role = discord.utils.get(member.guild.roles, id=role[0])
+			except Exception:
+				return
+			if role != None:
+				await member.add_roles(role, reason='Auto-Role')
 			
 
 def setup(bot):
