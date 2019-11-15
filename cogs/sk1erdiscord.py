1523621a04ae363a04ccf83bfea24819db32a696
GamingGeek <thegaminggeek362@gmail.com>
2019-11-15 22:40:20 +0000

    add testing role

--- cogs/sk1erdiscord.py
+++ cogs/sk1erdiscord.py
@@ -53,7 +53,8 @@ class sk1ercog(commands.Cog, name="Sk1er's Epic Cog"):
 				aroles.append(role.name)
 			s = set(aroles)
 			removed = [x for x in broles if x not in s]
-			if self.nitro in removed:
+			test = discord.utils.get(self.guild.roles, id=645029907977601034)
+			if self.nitro in removed or test in removed:
 				async with aiohttp.ClientSession(headers=self.headers) as session:
 					async with session.get(f'https://api.github.com/gists/{self.gist}') as resp:
 						if resp.status != 200:
