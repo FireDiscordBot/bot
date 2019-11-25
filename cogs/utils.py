fe9824c4834e80b20241f113d7137e1e5db5cfeb
GamingGeek <thegaminggeek362@gmail.com>
2019-11-25 16:12:15 +0000

    ctx. is important part 2

--- cogs/utils.py
+++ cogs/utils.py
@@ -1226,7 +1226,7 @@ class utils(commands.Cog, name='Utility Commands'):
 				embed.set_thumbnail(url=str(ctx.guild.icon_url))
 			else:
 				image = ctx.guild.splash_url or ctx.guild.banner_url
-				if 'PARTNERED' in guild.features or 'VERIFIED' in guild.features:
+				if 'PARTNERED' in ctx.guild.features or 'VERIFIED' in ctx.guild.features:
 					if ctx.guild.splash_url:
 						splashraw = await ctx.guild.splash_url.read()
 					elif ctx.guild.banner_url:
