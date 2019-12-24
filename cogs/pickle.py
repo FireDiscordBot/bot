52a1aff9873f4860e9fc9ebbc96da605571aea58
Ender <xenderpugx@gmail.com>
2019-12-24 20:02:33 +0000

    Update pickle.py

--- cogs/pickle.py
+++ cogs/pickle.py
@@ -603,7 +603,27 @@ class pickle(commands.Cog, name="Hypixel Commands"):
 		embed.set_image(url=f'https://mc-heads.net/body/{uid}')
 		embed.set_footer(text=f'Requested by {ctx.author}', icon_url=str(ctx.author.avatar_url_as(static_format='png', size=2048)))
 		await ctx.send(embed=embed)
-
+					
+					
+					
+					
+					
+	@commands.command(description='View a player\'s Minecraft skin.')
+	async def skin(self, ctx, *, ign: str = None):
+		if not ign:
+			return await ctx.send('<a:fireFailed:603214400748257302> You must provide a name!')
+		uid = await self.nameToUUID(ign)
+		# timestamp = str(datetime.datetime.utcnow().timestamp()).split('.')[0]
+		embed = discord.Embed(color=ctx.author.color)
+		embed.set_image(url=f'https://mc-heads.net/body/{uid}')
+		embed.set_footer(text=f'Requested by {ctx.author}', icon_url=str(ctx.author.avatar_url_as(static_format='png', size=2048)))
+		await ctx.send(embed=embed)		
+					
+					
+					
+					
+					
+					
 	@commands.command(description='View the current status of Minecraft services')
 	async def mcstatus(self, ctx):
 		emotes = {
