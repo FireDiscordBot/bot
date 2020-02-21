bb6e67c4e639be5ecdde035a2503b04e41d29528
GamingGeek <thegaminggeek362@gmail.com>
2020-02-21 01:30:17 +0100

    bug fixes, added confirmation for close

--- modules/tickets.py
+++ modules/tickets.py
@@ -19,6 +19,7 @@ from fire.converters import Member, Category
 from discord.ext import commands
 import datetime
 import discord
+import asyncio
 import random
 import uuid
 import re
@@ -30,7 +31,7 @@ class tickets(commands.Cog, name="Tickets"):
         self.bot = bot
         self.words = open('./words.txt').read().split(' ')
 
-    @commands.group(name='tickets', description='View all the ticket configuration commands')
+    @commands.group(name='tickets', description='View all the ticket configuration commands', aliases=['ticket'])
     @commands.has_permissions(manage_channels=True)
     async def tickets_group(self, ctx):
         if ctx.invoked_subcommand:
@@ -52,13 +53,13 @@ class tickets(commands.Cog, name="Tickets"):
             value='Set the name for tickets. There are many variables available for use in the name',
             inline=False
         )
-        embed.set_author(str(ctx.author), icon_url=str(ctx.author.avatar_url_as(static_format='png')))
+        embed.set_author(name=str(ctx.author), icon_url=str(ctx.author.avatar_url_as(static_format='png')))
         return await ctx.send(embed=embed)
 
     @tickets_group.command(name='category', description='Set the category where tickets are made')
     @commands.has_permissions(manage_channels=True)
     async def tickets_category(self, ctx, category: discord.CategoryChannel = None):
-        await self.bot.config[ctx.guild.id].set('tickets.parent', category)
+        await self.bot.configs[ctx.guild.id].set('tickets.parent', category)
         if not category:
             return await ctx.success(f'Successfully disabled tickets.')
         return await ctx.success(f'Successfully enabled tickets and set the category to {category}.')
@@ -66,7 +67,7 @@ class tickets(commands.Cog, name="Tickets"):
     @tickets_group.command(name='limit', description='Set the limit for how many tickets a user can make')
     @commands.has_permissions(manage_channels=True)
     async def tickets_limit(self, ctx, limit: int = 0):
-        await self.bot.config[ctx.guild.id].set('tickets.limit', limit)
+        await self.bot.configs[ctx.guild.id].set('tickets.limit', limit)
         return await ctx.success(f'Successfully set the ticket limit to {limit}')
 
     @tickets_group.command(name='name', description='Set the name for tickets')
@@ -85,7 +86,7 @@ class tickets(commands.Cog, name="Tickets"):
             embed = discord.Embed(color=ctx.author.color, timestamp=datetime.datetime.utcnow())
             embed.add_field(name='Variables', value=variables, inline=False)
             return await ctx.send(embed=embed)
-        await self.bot.config[ctx.guild.id].set('tickets.name', name)
+        await self.bot.configs[ctx.guild.id].set('tickets.name', name)
         fname = name
         for k, v in variables.items():
             fname = fname.replace(k, str(v))
@@ -93,14 +94,14 @@ class tickets(commands.Cog, name="Tickets"):
 
     @commands.command(name='new', description='Makes a new ticket')
     @commands.bot_has_permissions(manage_channels=True, manage_roles=True)
-    async def tickets_new(self, ctx, *, subject: str = None):
+    async def tickets_new(self, ctx, *, subject: str = "No subject given"):
         creating = await ctx.send('Creating your ticket...')
         config = self.bot.configs[ctx.guild.id]
         parent = config.get('tickets.parent')
-        limit = config.get('tickets.limits')
+        limit = config.get('tickets.limit')
         if not parent:
             return await ctx.error('Tickets are not enabled here')
-        if limit and len([c for c in parent.channels if str(ctx.author.id) in c.topic]) > limit:
+        if limit and len([c for c in parent.channels if str(ctx.author.id) in str(c.topic)]) > limit:
             return await ctx.error('You have too many tickets open!')
         variables = {
             '{increment}': config.get('tickets.increment'),
@@ -115,13 +116,14 @@ class tickets(commands.Cog, name="Tickets"):
             name = name.replace(k, str(v)).replace('crab', '🦀')  # asbyth has me putting crabs everywhere
         overwrites = {
             ctx.author: discord.PermissionOverwrite(read_messages=True, send_messages=True),
+            ctx.guild.me: discord.PermissionOverwrite(read_messages=True, send_messages=True, manage_channels=True, manage_roles=True),
             ctx.guild.default_role: discord.PermissionOverwrite(read_messages=False)
         }
         overwrites.update(parent.overwrites)
         ticket = await parent.create_text_channel(
             name=name,
             overwrites=overwrites,
-            topic=f'Ticket created by {ctx.author} ({ctx.author.id})',
+            topic=f'Ticket created by {ctx.author} ({ctx.author.id}) with subject "{subject}"',
             reason=f'Ticket created by {ctx.author} ({ctx.author.id})'
         )
         embed = discord.Embed(
@@ -129,12 +131,13 @@ class tickets(commands.Cog, name="Tickets"):
             timestamp=datetime.datetime.utcnow(),
             color=ctx.author.color
         )
-        embed.add_field(name='Subject', value=subject or "No subject given")
+        embed.add_field(name='Subject', value=subject)
         await ticket.send(embed=embed)
         tchannels = [c for c in config.get('tickets.channels') if c]  # Removes any channels that no longer exist.
         tchannels.append(ticket)
         await config.set('tickets.channels', tchannels)
-        return await creating.edit(f'<:check:674359197378281472> Successfully made your ticket, {ticket.mention}')
+        await config.set('tickets.increment', config.get('tickets.increment') + 1)
+        return await creating.edit(content=f'<:check:674359197378281472> Successfully made your ticket, {ticket.mention}')
 
     @commands.command(name='add', description='Add a user to the current ticket')
     @commands.bot_has_permissions(manage_roles=True)
@@ -159,18 +162,25 @@ class tickets(commands.Cog, name="Tickets"):
             return await ctx.error('You cannot remove the ticket author')
         if not user.permissions_in(ctx.channel).read_messages:
             return await ctx.error(f'{user} is not here, so how are you gonna remove them? 🤔')
+        if user.permissions_in(ctx.channel).manage_channels:
+            return await ctx.error(f'You cannot remove this user')
         await ctx.channel.set_permissions(user, read_messages=False, send_messages=False)
         return await ctx.success(f'Successfully removed {user} from the ticket')
 
     @commands.command(name='close', description='Closes a ticket, uploads the transcript to action logs channel and sends to the ticket author')
     @commands.bot_has_permissions(manage_roles=True)
-    async def tickets_remove(self, ctx, *, reason: str = "No Reason Provided"):
+    async def tickets_close(self, ctx, *, reason: str = "No Reason Provided"):
         config = self.bot.configs[ctx.guild.id]
         tchannels = config.get('tickets.channels')
         if ctx.channel not in tchannels:
             return await ctx.error('This command can only be ran in ticket channels!')
         if not ctx.author.permissions_in(ctx.channel).manage_channels:
             return await ctx.error('You must have `Manage Channels` permission to close tickets')
+        await ctx.error(f'Are you sure you want to close this ticket? Type `close` to confirm')
+        try:
+            await self.bot.wait_for('message', check=lambda m: m.author == ctx.author and m.channel == ctx.channel and m.content.lower() == 'close', timeout=10)
+        except asyncio.TimeoutException:
+            return await ctx.error('No response, aborting close.')
         closing = await ctx.send('Closing ticket, this may make take a bit...')
         transcript = []
         async for m in ctx.channel.history(limit=None):
@@ -183,17 +193,18 @@ class tickets(commands.Cog, name="Tickets"):
                 await m.send(f'Your ticket in {ctx.guild} was closed for the reason "{reason}". The transcript is below',
                              file=discord.File(transcript, filename=f'{ctx.channel}-transcript.txt'))
         actionlogs = config.get('log.action')
-        embed = discord.Embed(
-            title=f'Ticket {ctx.channel} was closed',
-            timestamp=datetime.datetime.utcnow(),
-            color=author.color
-        )
-        embed.add_field(name='Closed by', value=f'{ctx.author} ({ctx.author.id})', inline=False)
-        embed.add_field(name='Reason', value=reason, inline=False)
-        await actionlogs.send(
-            embed=embed,
-            file=discord.File(transcript, filename=f'{ctx.channel}-transcript.txt')
-        )
+        if actionlogs:
+            embed = discord.Embed(
+                title=f'Ticket {ctx.channel} was closed',
+                timestamp=datetime.datetime.utcnow(),
+                color=author.color
+            )
+            embed.add_field(name='Closed by', value=f'{ctx.author} ({ctx.author.id})', inline=False)
+            embed.add_field(name='Reason', value=reason, inline=False)
+            await actionlogs.send(
+                embed=embed,
+                file=discord.File(transcript, filename=f'{ctx.channel}-transcript.txt')
+            )
         return await ctx.channel.delete(reason=f'Ticket closed by {ctx.author} for "{reason}"')
 
 
