8139437fc95fd2a9cfb294bc88ce146aa7efdd42
GamingGeek <thegaminggeek362@gmail.com>
2020-03-27 03:22:21 +0100

    added another string for log detection

--- cogs/sk1erdiscord.py
+++ cogs/sk1erdiscord.py
@@ -46,7 +46,8 @@ class sk1ercog(commands.Cog, name="Sk1er's Epic Cog"):
 			'launchermeta.mojang.com',
 			'Running launcher core',
 			'[Client thread/INFO]: Setting user:',
-			'[Client thread/INFO]: (Session ID is'
+			'[Client thread/INFO]: (Session ID is',
+			'MojangTricksIntelDriversForPerformance'
 		]
 		self.secrets = r'(club\.sk1er\.mods\.levelhead\.auth\.MojangAuth|api\.sk1er\.club\/auth|LoginPacket|SentryAPI\.cpp|"authHash":|"hash":"|--accessToken|\(Session ID is token:|Logging in with details: |Server-Hash: |Checking license key :)'
 		self.emailre = r'[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+'
