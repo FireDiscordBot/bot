9c15b782f6029745339f9e0af78ee7f42da296ff
GamingGeek <thegaminggeek362@gmail.com>
2020-04-12 16:46:06 +0200

    Token scanning and uploading

--- events/message.py
+++ events/message.py
@@ -21,7 +21,10 @@ from contextlib import suppress
 import functools
 import traceback
 import asyncio
+import aiohttp
 import discord
+import base64
+import json
 import re
 
 
@@ -33,6 +36,8 @@ class Message(commands.Cog):
         self.dupecheck = {}
         self.uuidregex = r"[a-f0-9]{8}-?[a-f0-9]{4}-?4[a-f0-9]{3}-?[89ab][a-f0-9]{3}-?[a-f0-9]{12}"
         self.urlregex = r'(?:https:\/\/|http:\/\/)[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&\/\/=]*)'
+        self.tokenregex = r'[MN][A-Za-z\d]{23}\.[\w-]{6}\.[\w-]{27}'
+        self.gistheaders = {'Authorization': f'token {self.bot.config["github"]}'}
 
     def uuidgobyebye(self, text: str):
         return re.sub(self.uuidregex, '', text, 0, re.MULTILINE)
@@ -46,8 +51,54 @@ class Message(commands.Cog):
         except Exception:
             pass
 
+    async def token_gist(self, tokens, message):
+        files = {}
+        for t in tokens:
+            uid = int(base64.b64decode(t.split('.')[0]).decode('utf-8'))
+            user = 'Unknown User#0000'
+            try:
+                user = await self.bot.fetch_user(uid)
+            except Exception:
+                pass
+            files[f'{str(user).split("#")[0]}.txt'] = {'content':f'''
+Oh no, it seems a token has been leaked! Fire (Fire#0682) scans for tokens in Discord messages and uploads them to GitHub to be reset.
+You can learn more about GitHub's token scanning at https://help.github.com/en/github/administering-a-repository/about-token-scanning
+
+The token in question was found in a message sent by {message.author} in the channel #{message.channel} in the guild {message.guild}.
+I have attempted to identify who the token belongs to and found {user}
+
+The token in question is {t}
+
+Discord should send the owner of this bot a system message letting them know their token has been leaked pretty much instantly.
+If you are the owner of said bot, you should look into how this happened and try prevent it in the future!
+
+Examples of how you can protect your token is by using a config file or environment variables. This ensures the token is not directly in your code.
+
+I hope you keep your token safe in the future :)
+
+If you have any queries about this gist, feel free to email tokens@gaminggeek.dev
+'''}
+        body = {
+            'description': 'A token was found in a Discord message by Fire#0682',
+            'public': True,
+            'files': files
+        }
+        async with aiohttp.ClientSession(headers=self.gistheaders) as session:
+            async with session.post('https://api.github.com/gists', json=body) as r:
+                if r.status != 201:
+                    self.bot.logger.warn(f'Failed to create gist for tokens! Status: {r.status}')
+            await session.close()
+
+
     @commands.Cog.listener()
     async def on_message(self, message):
+        embeds = [str(e.to_dict()) for e in message.embeds]
+        tokens = re.findall(self.tokenregex, message.system_content or str(embeds), re.MULTILINE)
+        if tokens:
+            try:
+                await self.token_gist(tokens, message)
+            except Exception as e:
+                self.bot.logger.warn(f'Failed to upload token to gist (to reset ofc)', exc_info=e)
         if not isinstance(message.author, discord.Member):
             return
         if message.author.bot:
