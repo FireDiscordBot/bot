8001e425d5c1e99e2a2ff7c1442c1d088d1f27cd
GamingGeek <geek@gaminggeek.dev>
2020-04-20 21:29:48 +0100

    Add more endpoints to top.gg module

--- modules/topgg.py
+++ modules/topgg.py
@@ -46,6 +46,72 @@ class TopGG(commands.Cog):
         )
         self.bot.logger.info(f'$GREENPosted guild count $CYAN({len(self.bot.guilds)}) $GREENto $CYANtop.gg')
 
+    async def get_bots(self, **kwargs):
+        params = ['limit', 'offset', 'search', 'sort', 'fields']
+        for k in kwargs:
+            if k not in params:
+                kwargs.pop(k)
+        route = Route(
+            'GET',
+            f'/bots'
+        )
+        bots = await self.http.request(
+            route,
+            params=kwargs
+        )
+        return bots
+
+    async def get_bot(self, bot: int):
+        route = Route(
+            'GET',
+            f'/bots/{bot}'
+        )
+        bot = await self.http.request(
+            route
+        )
+        return bot
+
+    async def get_votes(self):
+        route = Route(
+            'GET',
+            f'/bots/{self.bot.user.id}/votes'
+        )
+        votes = await self.http.request(
+            route
+        )
+        return votes
+
+    async def check_vote(self, user: int):
+        route = Route(
+            'GET',
+            f'/bots/{self.bot.user.id}/check'
+        )
+        vote = await self.http.request(
+            route,
+            params={'userid':user}
+        )
+        return bool(vote.get('voted', 0))
+
+    async def get_stats(self, bot: int):
+        route = Route(
+            'GET',
+            f'/bots/{bot}/stats'
+        )
+        stats = await self.http.request(
+            route
+        )
+        return stats
+
+    async def is_weekend(self):
+        route = Route(
+            'GET',
+            f'/weekend'
+        )
+        weekend = await self.http.request(
+            route
+        )
+        return weekend.get('is_weekend', False)
+
 
 def setup(bot):
     if not bot.dev:
