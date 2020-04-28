c31714ae6fdab02a98f62d2c3187eae75aa9d974
GamingGeek <geek@gaminggeek.dev>
2020-04-28 21:28:07 +0100

    split skier cog into separate command files

---
+++ commands/levelhead.py
@@ -0,0 +1,121 @@
+"""
+MIT License
+Copyright (c) 2020 GamingGeek
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+and associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+
+
+from discord.ext import commands
+from fire.http import Route
+from io import BytesIO
+from PIL import Image
+from cogs import mcfont
+import traceback
+import datetime
+import aiohttp
+import discord
+import re
+
+
+remcolor = r'&[0-9A-FK-OR]'
+
+
+class Levelhead(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(description="Get a player's levelhead info")
+    async def levelhead(self, ctx, player: str = None):
+        if player is None:
+            return await ctx.send("What user should I check?")
+        route = Route(
+            'GET',
+            f'/levelheadv5/{player}/LEVEL'
+        )
+        try:
+            levelhead = await self.bot.http.sk1er.request(route)
+        except Exception:
+            return await ctx.error(f'Failed to fetch levelhead info')
+        uuid = levelhead.get('uuid', '')
+        if not uuid:
+            strlevel = levelhead['strlevel']
+            try:
+                hcolor = levelhead['header_obj']['color']
+                fcolor = levelhead['footer_obj']['color']
+            except KeyError:
+                hcolor = '§b'
+                fcolor = '§e'
+            fulllvlhead = f'{hcolor}Level: {fcolor}{levelhead["level"]}'
+            parsedtxt = mcfont.parse(fulllvlhead)
+            width = mcfont.get_width(parsedtxt)
+            img = Image.new('RGBA', (width + 25, 42))
+            mcfont.render((5, 0), parsedtxt, img)
+            buf = BytesIO()
+            img.save(buf, format='PNG')
+            buf.seek(0)
+            customlvl = discord.File(buf, 'mitchplshireme.png')
+            embed = discord.Embed(title=f"{player}'s Levelhead", colour=ctx.author.color, url="https://purchase.sk1er.club/category/1050972", timestamp=datetime.datetime.utcnow())
+            embed.add_field(name="IGN", value=player, inline=False)
+            embed.set_image(url='attachment://mitchplshireme.png')
+            return await ctx.send(embed=embed, file=customlvl)
+        if len(uuid) < 28:
+            return await ctx.error('Malformed UUID. Check the spelling of the player\'s name')
+        route = Route(
+            'GET',
+            f'/levelhead_purchase_status/{uuid}'
+        )
+        try:
+            purchase = await self.bot.http.sk1er.request(route)
+        except Exception:
+            return await ctx.error(f'Failed to fetch levelhead purchase status')
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f'https://api.hyperium.cc/levelhead_propose/{uuid}') as resp:
+                await session.close()
+                try:
+                    proposal = await resp.json()
+                except Exception:
+                    proposal = None
+        header = re.sub(remcolor, '', levelhead.get('header', 'Level'), 0, re.IGNORECASE)
+        strlevel = re.sub(remcolor, '', levelhead['strlevel'], 0, re.IGNORECASE)
+        level = levelhead['level']
+        nocustom = True if header == "Level" else False
+        tab = 'Purchased!' if purchase.get('tab', False) else 'Not Purchased'
+        chat = 'Purchased!' if purchase.get('chat', False) else 'Not Purchased'
+        head = purchase['head']
+        embed = discord.Embed(title=f"{player}'s Levelhead", colour=ctx.author.color, url="https://purchase.sk1er.club/category/1050972", timestamp=datetime.datetime.utcnow())
+        embed.set_footer(text="Want more integrations? Use the suggest command to suggest some")
+        if nocustom:
+            embed.add_field(name="IGN", value=player, inline=False)
+            embed.add_field(name="Levelhead", value=f"Level: {levelhead['level']}", inline=False)
+            embed.add_field(name="Other items", value=f"Tab: {tab} \nChat: {chat} \nAddon Head Layers: {head}", inline=False)
+        else:
+            embed.add_field(name="Custom Levelhead?", value="Yeah!", inline=False)
+            embed.add_field(name="IGN", value=player, inline=False)
+            embed.add_field(name="Levelhead", value=f"{header}:{strlevel}", inline=False)
+            if proposal:
+                nheader = re.sub(remcolor, '', proposal['header'], 0, re.IGNORECASE)
+                nstrlevel = re.sub(remcolor, '', proposal['strlevel'], 0, re.IGNORECASE)
+                embed.add_field(name='Proposed Levelhead', value=f'{nheader}:{nstrlevel}', inline=False)
+                embed.add_field(name='Denied?', value=proposal['denied'], inline=False)
+            embed.add_field(name="Other items", value=f"Tab: {tab} \nChat: {chat} \nAddon Head Layers: {head}", inline=False)
+        return await ctx.send(embed=embed)
+
+
+def setup(bot):
+    try:
+        bot.add_cog(Levelhead(bot))
+        bot.logger.info(f'$GREENLoaded $CYAN"levelhead" $GREENcommand!')
+    except Exception as e:
+        # errortb = ''.join(traceback.format_exception(type(e), e, e.__traceback__))
+        bot.logger.error(f'$REDError while adding command $CYAN"levelhead"', exc_info=e)
---
+++ commands/mod.py
@@ -0,0 +1,96 @@
+"""
+MIT License
+Copyright (c) 2020 GamingGeek
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+and associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+
+
+from jishaku.paginators import WrappedPaginator, PaginatorEmbedInterface
+from discord.ext import commands
+from fire.http import Route
+import traceback
+import datetime
+import discord
+
+
+class Mod(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name='mod', description="Get info about a Sk1er mod", aliases=['mods'])
+    async def skiermod(self, ctx, *, mod: str = None):
+        if mod is None:
+            return await ctx.error("You need to provide a mod name")
+        route = Route(
+            'GET',
+            f'/mods'
+        )
+        try:
+            mods = await self.bot.http.sk1er.request(route)
+        except Exception:
+            return await ctx.error(f'Failed to fetch mods')
+        names = {}
+        for m in mods:
+            names[mods[m]['display'].lower()] = m
+            for mod_id in mods[m]['mod_ids']:
+                names[mod_id.lower()] = m
+        if mod.lower() not in names:
+            return await ctx.error(f'Unknown mod.')
+        else:
+            mod = mods[names[mod.lower()]]
+        route = Route(
+            'GET',
+            f'/mods_analytics'
+        )
+        try:
+            analytics = await self.bot.http.sk1er.request(route)
+            analytics = analytics.get(mod['mod_ids'][0], {})
+        except Exception:
+            return await ctx.error(f'Failed to fetch mod analytics')
+        embed = discord.Embed(
+            title=mod['display'],
+            colour=ctx.author.color,
+            url=f"https://sk1er.club/mods/{mod['mod_ids'][0]}",
+            description=mod['short'],
+            timestamp=datetime.datetime.utcnow()
+        )
+        embed.add_field(name="Versions", value='\n'.join([f'**{k}**: {v}' for k, v in mod['latest'].items()]), inline=False)
+        embed.add_field(name="Creator", value=f'''**__{mod['vendor']['name']}__**
+[Website]({mod['vendor']['website']})
+[Twitter]({mod['vendor']['twitter']})
+[YouTube]({mod['vendor']['youtube']})
+''', inline=False)
+        if analytics:
+            embed.add_field(name="Analytics", value=f"Total: {analytics['total']:,d}, Online: {analytics['online']:,d}, Last Day: {analytics['day']:,d}, Last Week: {analytics['week']:,d}", inline=False)
+        await ctx.send(embed=embed)
+        paginator = WrappedPaginator(prefix='', suffix='', max_size=490)
+        for mcv in mod['changelog']:
+            paginator.add_line(f'**__{mcv}__**')
+            for v in mod['changelog'][mcv]:
+                changelog = mod["changelog"][mcv][v][0]
+                time = datetime.datetime.utcfromtimestamp(changelog["time"] / 1000).strftime('%d/%m/%Y @ %I:%M:%S %p')
+                paginator.add_line(f'**{v}**: {changelog["text"]} ({time})')
+            paginator.add_line('-----------------')
+        embed = discord.Embed(color=ctx.author.color, title='Changelogs', timestamp=datetime.datetime.utcnow())
+        interface = PaginatorEmbedInterface(ctx.bot, paginator, owner=ctx.author, _embed=embed)
+        await interface.send_to(ctx)
+
+
+def setup(bot):
+    try:
+        bot.add_cog(Mod(bot))
+        bot.logger.info(f'$GREENLoaded $CYAN"Mod" $GREENcommand!')
+    except Exception as e:
+        # errortb = ''.join(traceback.format_exception(type(e), e, e.__traceback__))
+        bot.logger.error(f'$REDError while adding command $CYAN"Mod"', exc_info=e)
