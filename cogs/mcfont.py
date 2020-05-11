a8dd7b26aa8fdb36ed10de23ebbe4efcdf6e04f8
GamingGeek <geek@gaminggeek.dev>
2020-05-11 02:35:08 +0200

    Make get_width cleaner

--- cogs/mcfont.py
+++ cogs/mcfont.py
@@ -182,23 +182,7 @@ def get_width(message):
     The message has to be in the format returned by the parse funtion
     '''
     w = 0
-    ws = {
-        1: 0,
-        2: 0,
-        3: 0,
-        4: 0,
-        5: 0,
-        6: 0,
-        7: 0,
-        8: 0,
-        9: 0,
-        10: 0,
-        11: 0,
-        12: 0,
-        13: 0,
-        14: 0,
-        15: 0
-    }
+    ws = {i: 0 for i in range(1, 31)}
     highestw = 0
     for i in message:
         if i[-1] == 0:
