f0879c8d82e4bee020d4d1adfe7e49b301e47225
GamingGeek <geek@gaminggeek.dev>
2020-06-27 19:12:13 +0200

    aiohttp moment

--- commands/mcstatus.py
+++ commands/mcstatus.py
@@ -19,6 +19,7 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 from discord.ext import commands
 import traceback
 import discord
+import aiohttp
 
 
 class MCStatus(commands.Cog):
