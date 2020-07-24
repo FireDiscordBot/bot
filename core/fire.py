920aa3c3db62aaa1b91c8eb35d006ce225cc4311
GamingGeek <geek@gaminggeek.dev>
2020-07-24 13:53:45 +0100

    temp disable cw

--- core/fire.py
+++ core/fire.py
@@ -207,8 +207,8 @@ class Fire(commands.Bot):
 
     def load_commands(self):
         try:
-            # raise Exception('Chatwatch is temporarily disabled')
-            self.load_extension('core.chatwatch')
+            raise Exception('Chatwatch is temporarily disabled')
+            # self.load_extension('core.chatwatch')
         except Exception as e:
             # errortb = ''.join(traceback.format_exception(
             #     type(e), e, e.__traceback__))
