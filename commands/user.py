ee9ed40a467a52856ed24b566cc5ab0b1247952c
GamingGeek <geek@gaminggeek.dev>
2020-08-06 15:32:19 +0200

    fix badge checks

--- commands/user.py
+++ commands/user.py
@@ -64,28 +64,30 @@ class UserInfo(commands.Cog):
 
     def get_badges(self, user: typing.Union[discord.User, discord.Member]):
         badge_checks = {
-            self.bot.isadmin(user): discord.utils.get(
-                self.bot.emojis, id=671243744774848512),
-            user.public_flags.staff: discord.utils.get(
+            'staff': discord.utils.get(
                 self.bot.emojis, id=698344463281422371),
-            user.public_flags.partner: discord.utils.get(
+            'partner': discord.utils.get(
                 self.bot.emojis, id=631831109575114752),
-            user.public_flags.hypesquad: discord.utils.get(
+            'hypesquad': discord.utils.get(
                 self.bot.emojis, id=698349980192079882),
-            user.public_flags.bug_hunter: discord.utils.get(
+            'bug_hunter': discord.utils.get(
                 self.bot.emojis, id=698350213596971049),
-            user.public_flags.bug_hunter_level_2: discord.utils.get(
+            'bug_hunter_level_2': discord.utils.get(
                 self.bot.emojis, id=698350544103669771),
-            user.public_flags.early_supporter: discord.utils.get(
+            'early_supporter': discord.utils.get(
                 self.bot.emojis, id=698350657073053726),
-            user.public_flags.verified_bot: str(discord.utils.get(self.bot.emojis, id=700325427998097449)) +
+            'verified_bot': str(discord.utils.get(self.bot.emojis, id=700325427998097449)) +
             str(discord.utils.get(self.bot.emojis, id=700325521665425429)),
-            user.public_flags.verified_bot_developer: discord.utils.get(
-                self.bot.emojis, id=720179031785340938),
-            user.id in self.bot.premium_guilds.values(): discord.utils.get(
-                self.bot.emojis, id=680519037704208466)
+            'verified_bot_developer': discord.utils.get(
+                self.bot.emojis, id=720179031785340938)
         }
-        badges = [v for k, v in badge_checks.items() if k]
+        badges = [str(v) for k, v in badge_checks.items() if getattr(user.public_flags, k)]
+        if self.bot.isadmin(user):
+            badges.append(str(discord.utils.get(
+                self.bot.emojis, id=671243744774848512)))
+        if user.id in self.bot.premium_guilds.values():
+            badges.append(str(discord.utils.get(
+                self.bot.emojis, id=680519037704208466)))
         if badges:
             badges.append(u'\u200b')  # Prevents huge emojis on mobile
         return badges
