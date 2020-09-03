6796e65257f4e337786ed4a92673cc35dbdf95d8
Jake Ward <geek@gaminggeek.dev>
2020-09-03 20:29:48 +0100

    custom listener class

---
+++ lib/util/listener.ts
@@ -0,0 +1,13 @@
+import { Listener as AkairoListener, ListenerOptions } from "discord-akairo";
+import { Fire } from "../Fire";
+
+export class Listener extends AkairoListener {
+    client: Fire
+    constructor(id: string, options?: ListenerOptions) {
+        super(id, options)
+    }
+
+    async init() {
+        
+    }
+}
