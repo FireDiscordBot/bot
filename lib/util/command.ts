5e0099a6acbd81e13f98f17327987c48f85c277f
Jake Ward <geek@gaminggeek.dev>
2020-09-03 21:49:17 +0100

    fix aliases being necessary for commands to work

--- lib/util/command.ts
+++ lib/util/command.ts
@@ -2,12 +2,12 @@ import { Command as AkairoCommand, CommandOptions } from "discord-akairo";
 import { Fire } from "../Fire";
 
 export class Command extends AkairoCommand {
-    client: Fire
-    constructor(id: string, options?: CommandOptions) {
-        super(id, options)
-    }
+  client: Fire;
+  constructor(id: string, options?: CommandOptions) {
+    if (!options.aliases) options.aliases = [id];
+    else options.aliases.push(id);
+    super(id, options);
+  }
 
-    async init() {
-        
-    }
+  async init() {}
 }
