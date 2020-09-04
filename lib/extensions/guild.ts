795eb777f669d454819ab5ab0178be8a7bae800a
Jake Ward <geek@gaminggeek.dev>
2020-09-04 16:58:09 +0100

    Add language to FireGuild

--- lib/extensions/guild.ts
+++ lib/extensions/guild.ts
@@ -1,10 +1,15 @@
 import { Structures, Guild, Collection, Snowflake, User } from "discord.js";
 import { Fire } from "../Fire";
+import { Language } from "../util/language";
 
 export class FireGuild extends Guild {
   client: Fire;
+  language: Language;
   constructor(client: Fire, data: object) {
     super(client, data);
+    this.language = client.languages.modules.get(
+      client.settings.get(this.id, "utils.language", "en-US")
+    ) as Language;
   }
 
   resolveOrFetchUser(
