9d5a88126a21e50c5a86675e225b5ed8a3db62d3
Bedlessly <jejojora2@gmail.com>
2020-09-04 12:03:49 -0400

    Add vote command

---
+++ src/commands/vote.ts
@@ -0,0 +1,18 @@
+import { FireMessage } from "../../lib/extensions/message";
+import { Language } from "../../lib/util/language";
+import { Command } from "../../lib/util/command";
+
+export default class extends Command {
+  constructor() {
+    super("vote", {
+      description: (language: Language) =>
+        language.get("VOTE_COMMAND_DESCRIPTION"),
+      clientPermissions: ["SEND_MESSAGES"],
+      aliases: ["dbl", "top.gg", "dboats", "discord.boats"],
+    });
+  }
+
+  async exec(message: FireMessage, args: { name: string }) {
+    await message.channel.send("https://fire-is-the.best/vote");
+  }
+}
