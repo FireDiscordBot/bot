15a3a0d427e349e3f0b1f0cd18c26c6b45bc195e
Jake Ward <geek@gaminggeek.dev>
2020-09-04 16:18:17 +0100

    search members too

--- lib/util/clientUtil.ts
+++ lib/util/clientUtil.ts
@@ -1,5 +1,6 @@
 import { ClientUtil } from "discord-akairo";
 import { User } from "discord.js";
+import { Guild } from "discord.js";
 import { Snowflake } from "discord.js";
 import { Collection } from "discord.js";
 import { Fire } from "../Fire";
@@ -13,19 +14,29 @@ export class Util extends ClientUtil {
   async resolveOrFetchUser(
     text: string,
     users?: Collection<Snowflake, User>,
+    guild?: Guild,
     caseSensitive: boolean = false,
     wholeWord: boolean = true
   ) {
     let user: User;
-    if (text.match(/([0-9]{15,21})$/m))
+    if (text && text.match(/([0-9]{15,21})$/m))
       user = await this.client.users.fetch(text).catch(() => null);
-    if (!user)
+    if (!user) {
+      if (guild) {
+        let member = guild.members.cache.find(
+          (member) =>
+            member.displayName?.toLowerCase() == text.toLowerCase() ||
+            member.user.username?.toLowerCase() == text.toLowerCase()
+        );
+        if (member) return member.user;
+      }
+    } else
       return super.resolveUser(
         text,
         users || this.client.users.cache,
         caseSensitive,
         wholeWord
       );
-    else return user;
+    return user;
   }
 }
