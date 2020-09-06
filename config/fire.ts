935f5026963f4989e9c419e8e1a4005ae8229f40
Jake Ward <geek@gaminggeek.dev>
2020-09-06 23:18:47 +0100

    Formatting and remove "version" & add "dev" to config

--- config/fire.ts
+++ config/fire.ts
@@ -1,18 +1,19 @@
-import { version } from "../package.json";
 import { Fire } from "../lib/Fire";
 import * as moment from "moment";
 
 export const fire = {
-  version,
+  dev: process.env.NODE_ENV == "development",
   readyMessage: (client: Fire) => {
     client.console.log("-------------------------");
-    client.console.log(`Bot: ${client.user.username}#${client.user.discriminator}`);
-    client.console.log(`ID: ${client.user.id}`);
+    client.console.log(
+      `Bot: ${client?.user?.username}#${client?.user?.discriminator}`
+    );
+    client.console.log(`ID: ${client?.user?.id}`);
     client.console.log(`Guilds: ${client.guilds.cache.size}`);
     if (!client.started) {
-      const now = moment()
+      const now = moment();
       const duration = moment.duration(client.launchTime.diff(now));
-      client.console.log(`Started in ${duration.humanize()}`)
+      client.console.log(`Started in ${duration.humanize()}`);
     }
     client.console.log("-------------------------");
   },
