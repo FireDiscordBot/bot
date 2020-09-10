e4e6eb2cb8e9bb01310bbe999283031f156bda32
Jake Ward <geek@gaminggeek.dev>
2020-09-10 20:02:12 +0100

    actually return the damn string

--- lib/util/constants.ts
+++ lib/util/constants.ts
@@ -76,6 +76,7 @@ export const titleCase = (string: string) => {
   for (var i = 0; i < sentence.length; i++) {
     sentence[i] = sentence[i][0].toUpperCase() + sentence[i].slice(1);
   }
+  return sentence.join("");
 };
 
 export const zws = "\u200b";
