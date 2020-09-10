94ab4966d21b367c692a521d875e0f70fae3d012
Jake Ward <geek@gaminggeek.dev>
2020-09-10 20:27:27 +0100

    8ball command

---
+++ src/commands/eightball.ts
@@ -0,0 +1,19 @@
+import { FireMessage } from "../../lib/extensions/message";
+import { Language } from "../../lib/util/language";
+import { Command } from "../../lib/util/command";
+
+export default class Eightball extends Command {
+  constructor() {
+    super("8ball", {
+      description: (language: Language) =>
+        language.get("EIGHTBALL_COMMAND_DESCRIPTION"),
+      clientPermissions: ["SEND_MESSAGES"],
+    });
+  }
+
+  async exec(message: FireMessage) {
+    if (!message.content.trim().endsWith("?"))
+      return await message.send("EIGHTBALL_NO_QUESTION");
+    await message.send("EIGHTBALL_ANSWER");
+  }
+}
