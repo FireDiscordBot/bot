f394ab426a9b3c63f388510df155f5eba276740d
Jake Ward <geek@gaminggeek.dev>
2020-09-10 19:51:07 +0100

    Add titleCase function

--- lib/util/constants.ts
+++ lib/util/constants.ts
@@ -72,4 +72,11 @@ export const constants = {
 
 export const noop = () => {};
 
+export const titleCase = (string: string) => {
+  var sentence = string.toLowerCase().split(" ");
+  for (var i = 0; i < sentence.length; i++) {
+    sentence[i] = sentence[i][0].toUpperCase() + sentence[i].slice(1);
+  }
+};
+
 export const zws = "\u200b";
