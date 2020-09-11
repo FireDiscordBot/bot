d0da3f491a2ebabb7003172bc84c03d2425a8d42
Jake Ward <geek@gaminggeek.dev>
2020-09-11 16:24:25 +0100

    fix titleCase

--- lib/util/constants.ts
+++ lib/util/constants.ts
@@ -80,7 +80,7 @@ export const titleCase = (string: string) => {
   for (var i = 0; i < sentence.length; i++) {
     sentence[i] = sentence[i][0].toUpperCase() + sentence[i].slice(1);
   }
-  return sentence.join("");
+  return sentence.join(" ");
 };
 
 export const zws = "\u200b";
