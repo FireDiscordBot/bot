e18481ff305aae6e0289ad81ca6bb45a4e05b658
Jake Ward <geek@gaminggeek.dev>
2020-09-11 18:43:53 +0100

    hidden option for commands

--- lib/util/command.ts
+++ lib/util/command.ts
@@ -1,15 +1,24 @@
-import { Command as AkairoCommand, CommandOptions } from "discord-akairo";
+import {
+  Command as AkairoCommand,
+  CommandOptions as AkairoOptions,
+} from "discord-akairo";
 import { Fire } from "../Fire";
 
 export class Command extends AkairoCommand {
   client: Fire;
+  hidden: boolean;
   constructor(id: string, options?: CommandOptions) {
     if (!options?.aliases) options.aliases = [id];
     else options?.aliases?.push(id);
     super(id, options);
+    this.hidden = options.hidden || false;
   }
 
   async init() {}
 
   async unload() {}
+}
+
+interface CommandOptions extends AkairoOptions {
+  hidden?: boolean;
 }
