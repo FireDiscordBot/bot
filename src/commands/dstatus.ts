1a4f5dde55426700a13514d33a3ff2dcefd3a140
Jake Ward <geek@gaminggeek.dev>
2020-09-11 16:24:40 +0100

    change foreach

--- src/commands/dstatus.ts
+++ src/commands/dstatus.ts
@@ -1,10 +1,9 @@
+import { Component, Incidents, Summary } from "../../lib/interfaces/statuspage";
 import { constants, titleCase } from "../../lib/util/constants";
 import { FireMessage } from "../../lib/extensions/message";
 import { Language } from "../../lib/util/language";
 import { Command } from "../../lib/util/command";
-import { Request } from "centra";
 import * as centra from "centra";
-import { Incidents, Summary } from "../../lib/interfaces/statuspage";
 
 export default class DiscordStatus extends Command {
   constructor() {
@@ -31,42 +30,53 @@ export default class DiscordStatus extends Command {
           .send()
       ).json();
     } catch (e) {
-      this.client.sentry.captureException(e);
       return await message.send("DSTATUS_FETCH_FAIL");
     }
     let components = [];
     let groups = {};
     summary.components.forEach((component) => {
       if (
-        component?.group_id &&
         !(
           ["jk03xttfcz9b", "ghlgk5p8wyt7"].includes(component.group_id) &&
           component.status == "operational"
         )
       ) {
-        if (!Object.keys(groups).includes(component.group_id))
+        if (!Object.keys(groups).includes(component.group_id)) {
           groups[component.group_id] = [component];
-        else groups[component.group_id]?.push(component);
-      } else if (!component?.group_id) {
+        } else groups[component.group_id].push(component);
+      }
+    });
+    summary.components.forEach((component) => {
+      if (!component.group_id) {
         components.push(
           `├${constants.statuspage.emojis[component.status]} **${
             component.name
-          }**: ${titleCase(component.status.replace("_", " "))}`
+          }**: ${
+            message.language.get("STATUSPAGE_COMPONENT_STATUS")[
+              component.status.toLowerCase()
+            ] || titleCase(component.status.replace("_", " "))
+          }`
         );
-        if (groups.hasOwnProperty(component.id)) {
-          groups[component.id].forEach((groupComponent) => {
+        if (Object.keys(groups).includes(component.id))
+          groups[component.id].forEach((groupComponent: Component) => {
             components.push(
               `├─${constants.statuspage.emojis[groupComponent.status]} **${
                 groupComponent.name
-              }**: ${titleCase(groupComponent.status.replace("_", " "))}`
+              }**: ${
+                message.language.get("STATUSPAGE_COMPONENT_STATUS")[
+                  groupComponent.status.toLowerCase()
+                ] || titleCase(groupComponent.status.replace("_", " "))
+              }`
             );
           });
-        }
       }
     });
     const latest = incidents.incidents[0];
     let embed = {
-      title: summary.status.description,
+      title:
+        message.language.get("STATUSPAGE_PAGE_DESCRIPTIONS")[
+          summary.status.description.toLowerCase()
+        ] || titleCase(summary.status.description),
       description: components.join("\n"),
       color:
         constants.statuspage.colors[summary.status.indicator] ||
@@ -74,10 +84,14 @@ export default class DiscordStatus extends Command {
         "#ffffff",
       fields: [
         {
-          value: `[${latest.name}](${latest.shortlink})\nStatus: **${titleCase(
-            latest.status
-          )}**`,
-          name: "Latest Incident",
+          value: `[${latest.name}](${latest.shortlink})\n${message.language.get(
+            "STATUS"
+          )}: **${
+            message.language.get("STATUSPAGE_INCIDENT_STATUS")[
+              latest.status.toLowerCase()
+            ] || titleCase(latest.status)
+          }**`,
+          name: message.language.get("STATUS_LATEST_INCIDENT"),
           inline: true,
         },
       ],
