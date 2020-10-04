ac89e43f22485565972a9d9d68f4ef4b26295072
Jake Ward <geek@gaminggeek.dev>
2020-10-04 21:05:57 +0100

    Make shardCount & shards not optional

--- src/ws/events/LaunchEvent.ts
+++ src/ws/events/LaunchEvent.ts
@@ -7,7 +7,7 @@ export default class LaunchEvent extends Event {
     super(manager, EventType.LAUNCH_CLIENT);
   }
 
-  run(data: { shardCount?: number; shards?: number[] }) {
+  run(data: { shardCount: number; shards: number[] }) {
     this.manager.client.console.log("[Aether] Received launch event.");
     this.manager.launch(data || { shardCount: 1, shards: [0] });
   }
