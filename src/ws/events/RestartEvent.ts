782a8311d9c16e9cd5955a34a49c90b9af6b1671
Jake Ward <geek@gaminggeek.dev>
2020-10-04 22:48:23 +0100

    fix restartevent

--- src/ws/events/RestartEvent.ts
+++ src/ws/events/RestartEvent.ts
@@ -9,14 +9,17 @@ export default class RestartEvent extends Event {
 
   run(data: { shardCount: number; shards: number[] }) {
     this.manager.client.console.log(
-      "[Aether] Received restart event, relaunching client."
+      "[Aether] Received restart event, checking whether sharding options have changed..."
     );
     const currentOptions = this.manager.client.options;
     if (
       currentOptions.shardCount == data.shardCount &&
-      currentOptions.shards == data.shards
+      JSON.stringify(currentOptions.shards) == JSON.stringify(data.shards)
     )
       return;
+    this.manager.client.console.log(
+      "[Aether] Sharding options have changed, relaunching client."
+    );
     this.manager.relaunch(data || { shardCount: 1, shards: [0] });
   }
 }
