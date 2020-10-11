166ba172e2626fa1b0c348e1754b14d751d84e18
Jake Ward <geek@gaminggeek.dev>
2020-10-11 20:01:08 +0100

    fix import

--- src/arguments/command.ts
+++ src/arguments/command.ts
@@ -1,5 +1,6 @@
-import { ArgumentTypeCaster, Command } from "discord-akairo";
 import { FireMessage } from "../../lib/extensions/message";
+import { ArgumentTypeCaster } from "discord-akairo";
+import { Command } from "../../lib/util/command";
 
 export const commandTypeCaster: ArgumentTypeCaster = (
   message: FireMessage,
