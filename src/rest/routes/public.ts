dc4c6598a49dfa70c5f135077cb5bebaf7105cf1
Jake Ward <geek@gaminggeek.dev>
2020-10-11 22:12:38 +0100

    use Guild#isPublic

--- src/rest/routes/public.ts
+++ src/rest/routes/public.ts
@@ -1,13 +1,10 @@
+import { FireGuild } from "../../../lib/extensions/guild";
 import * as express from "express";
 
 export function publicRoute(req: express.Request, res: express.Response) {
   const client = req.app.client;
   const publicGuilds = client.guilds.cache
-    .filter(
-      (guild) =>
-        client.settings.get(guild.id, "utils.public", false) ||
-        guild.features.includes("DISCOVERABLE")
-    )
+    .filter((guild: FireGuild) => guild.isPublic())
     .map((guild) => guild.id);
   res.json(publicGuilds);
 }
