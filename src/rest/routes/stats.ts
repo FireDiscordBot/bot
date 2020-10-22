7db606276dd6c2bf914421937fa58ccd8e2cdff5
Jake Ward <geek@gaminggeek.dev>
2020-10-22 01:05:03 +0100

    hide publicGuilds & discoverableGuilds in stats endpoint

--- src/rest/routes/stats.ts
+++ src/rest/routes/stats.ts
@@ -2,5 +2,10 @@ import * as express from "express";
 
 export async function statsRoute(req: express.Request, res: express.Response) {
   const client = req.app.client;
-  res.json(await client.util.getClusterStats());
+  const stats = await client.util.getClusterStats();
+  stats.shards.forEach((shard) => {
+    delete shard.publicGuilds;
+    delete shard.discoverableGuilds;
+  }); // These are only here for Aether. The /public & /discoverable endpoints should be used to get this data
+  res.json(stats);
 }
