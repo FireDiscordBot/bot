087e3ef2cd6d7cda845f44cf90ef04bf5ba2a42c
Jake Ward <geek@gaminggeek.dev>
2020-10-22 05:24:28 +0100

    fix command categories

--- src/rest/routes/commands.ts
+++ src/rest/routes/commands.ts
@@ -1,6 +1,5 @@
 import { Command } from "../../../lib/util/command";
 import * as express from "express";
-import { getCommandArguments } from "../utils";
 
 interface Category {
   id: number;
@@ -18,36 +17,46 @@ export function commandsRoute(req: express.Request, res: express.Response) {
   const categories: Category[] = [];
   let categoryID = 0;
 
-  client.commandHandler.categories.forEach((commandsCategory) => {
-    const category: Category = {
-      id: categoryID++,
-      name: commandsCategory.id,
-      commands: [],
-    };
+  client.commandHandler.categories
+    .filter(
+      (category) =>
+        !!category.findKey(
+          (command) =>
+            command.category.id !== "Admin" &&
+            !command.hidden &&
+            !command.ownerOnly
+        )
+    )
+    .forEach((commandsCategory) => {
+      const category: Category = {
+        id: categoryID++,
+        name: commandsCategory.id,
+        commands: [],
+      };
 
-    category.commands = commandsCategory
-      .filter(
-        (command: Command) =>
-          !command.ownerOnly &&
-          command.category.id !== "Admin" &&
-          !command.hidden
-      )
-      .map((command) => {
-        const args = getCommandArguments(command).join(" ");
-        return {
-          name: command.id,
-          description: command.description(
-            client.languages.modules.get("en-US")
-          ),
-          usage: `{prefix}${command.id} ${args}`.trim(),
-          aliases: command.aliases.join(", "),
-        };
-      });
+      category.commands = commandsCategory
+        .filter(
+          (command: Command) =>
+            !command.ownerOnly &&
+            command.category.id !== "Admin" &&
+            !command.hidden
+        )
+        .map((command) => {
+          const args = command.getArgumentsClean().join(" ");
+          return {
+            name: command.id,
+            description: command.description(
+              client.languages.modules.get("en-US")
+            ),
+            usage: `{prefix}${command.id} ${args}`.trim(),
+            aliases: command.aliases.join(", "),
+          };
+        });
 
-    if (category.commands.length > 0) {
-      categories.push(category);
-    }
-  });
+      if (category.commands.length > 0) {
+        categories.push(category);
+      }
+    });
 
   res.json(categories);
 }
