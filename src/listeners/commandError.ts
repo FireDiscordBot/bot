10297d46e048c40a4d699c0af159ce69a9a9ec02
Jake Ward <geek@gaminggeek.dev>
2020-10-28 14:39:28 +0000

    fix commanderror

--- src/listeners/commandError.ts
+++ src/listeners/commandError.ts
@@ -1,6 +1,7 @@
 import { FireMessage } from "../../lib/extensions/message";
 import { Listener } from "../../lib/util/listener";
 import { Command } from "../../lib/util/command";
+import { GuildChannel } from "discord.js";
 import { TextChannel } from "discord.js";
 import { Scope } from "@sentry/node";
 
@@ -28,10 +29,13 @@ export default class CommandError extends Listener {
       });
       sentry.setExtras({
         "message.id": message.id,
-        "guild.id": message.guild.id,
-        "guild.name": message.guild.name,
+        "guild.id": message.guild?.id,
+        "guild.name": message.guild?.name,
         "channel.id": message.channel.id,
-        "channel.name": (message.channel as TextChannel).name,
+        "channel.name":
+          message.channel instanceof GuildChannel
+            ? (message.channel as TextChannel).name
+            : message.channel.recipient.toString(),
         "command.name": command.id,
         "command.args": JSON.stringify(args),
         env: process.env.NODE_ENV,
