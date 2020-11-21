f381f4cf23662ef1a8bfa71047d75ad4ab37dec0
Jake Ward <geek@gaminggeek.dev>
2020-11-21 16:48:10 +0000

    lol

--- src/ws/events/ReloadExperimentsEvent.ts
+++ src/ws/events/ReloadExperimentsEvent.ts
@@ -2,7 +2,7 @@ import { EventType } from "../../../lib/ws/util/constants";
 import { Event } from "../../../lib/ws/event/Event";
 import { Manager } from "../../../lib/Manager";
 
-export default class LaunchEvent extends Event {
+export default class ReloadExperimentsEvent extends Event {
   constructor(manager: Manager) {
     super(manager, EventType.RELOAD_EXPERIMENTS);
   }
