c51f8b7eea08889733ab1994f2b67fccddb1dd65
Jake Ward <thegaminggeek362@gmail.com>
2020-11-21 18:12:18 +0100

    remove logging for settings sync because spam go brrrrrrr

--- src/ws/events/SettingsSyncEvent.ts
+++ src/ws/events/SettingsSyncEvent.ts
@@ -8,11 +8,6 @@ export default class SettingsSyncEvent extends Event {
   }
 
   run(data: { user: string; setting: string; value: any }) {
-    this.manager.client.console.log(
-      `[Event] Received settings sync request for ${
-        this.manager.client.users.cache.get(data.user) || data.user
-      }.`
-    );
     if (data.value != "deleteSetting")
       this.manager.client.userSettings.set(data.user, data.setting, data.value);
     else this.manager.client.userSettings.delete(data.user, data.setting);
