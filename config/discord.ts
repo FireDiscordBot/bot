756571d5889d27902bcb208a7dc4a32f3574abf2
Jake Ward <geek@gaminggeek.dev>
2020-11-23 19:15:14 +0000

    enable voice states

--- config/discord.ts
+++ config/discord.ts
@@ -29,7 +29,8 @@ export const discord: ClientOptions = {
       intents.GUILD_INVITES |
       intents.GUILD_MESSAGES |
       intents.GUILD_MESSAGE_REACTIONS |
-      intents.DIRECT_MESSAGES,
+      intents.DIRECT_MESSAGES |
+      intents.GUILD_VOICE_STATES,
   },
   presence: {
     activity: {
