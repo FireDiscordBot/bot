9fe94f5f07c0eb1a79a5c21c4de545063771ed98
Jake Ward <geek@gaminggeek.dev>
2020-11-28 22:26:34 +0000

    disable presences again

--- config/discord.ts
+++ config/discord.ts
@@ -20,7 +20,7 @@ export const discord: ClientOptions = {
     intents:
       intents.GUILDS |
       intents.GUILD_MEMBERS |
-      intents.GUILD_PRESENCES |
+      // intents.GUILD_PRESENCES |
       intents.GUILD_BANS |
       intents.GUILD_INVITES |
       intents.GUILD_MESSAGES |
