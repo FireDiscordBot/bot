93458641a7425d8b28125f72022b90ca0c24dc31
Jake Ward <geek@gaminggeek.dev>
2020-12-10 06:39:27 +0000

    check if command is debug for accountage

--- src/inhibitors/accountage.ts
+++ src/inhibitors/accountage.ts
@@ -1,5 +1,6 @@
 import { FireMessage } from "../../lib/extensions/message";
 import { Inhibitor } from "../../lib/util/inhibitor";
+import { Command } from "../../lib/util/command";
 import * as moment from "moment";
 
 export default class AccountAgeInhibitor extends Inhibitor {
@@ -10,8 +11,11 @@ export default class AccountAgeInhibitor extends Inhibitor {
     });
   }
 
-  exec(message: FireMessage) {
-    if (moment(new Date()).diff(message.author.createdAt) < 86400000)
+  exec(message: FireMessage, command: Command) {
+    if (
+      moment(new Date()).diff(message.author.createdAt) < 86400000 &&
+      command.id != "debug"
+    )
       return true;
     return false;
   }
