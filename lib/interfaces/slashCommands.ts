820e1ec8265e5f520de89d76b204eaf7d77956d3
Jake Ward <geek@gaminggeek.dev>
2020-12-10 06:39:42 +0000

    slash command interfaces

---
+++ lib/interfaces/slashCommands.ts
@@ -0,0 +1,33 @@
+import { APIGuildMember as Member } from "discord-api-types";
+
+export interface Command {
+  type: number;
+  token: string;
+  member: Member;
+  id: string;
+  guild_id: string;
+  data: CommandData;
+  channel_id: string;
+}
+
+export interface CommandData {
+  options: Option[];
+  name: string;
+  id: string;
+}
+
+export interface Option {
+  name: string;
+  value: string | number | boolean;
+}
+
+export enum ApplicationCommandOptionType {
+  SUB_COMMAND = 1,
+  SUB_COMMAND_GROUP = 2,
+  STRING = 3,
+  INTEGER = 4,
+  BOOLEAN = 5,
+  USER = 6,
+  CHANNEL = 7,
+  ROLE = 8,
+}
