adf4578ed49f81c3e88cf867a231753fffa34b77
Jake Ward <geek@gaminggeek.dev>
2020-12-10 06:39:16 +0000

    add shard check to guildCreate & guildDelete

--- src/listeners/guildCreate.ts
+++ src/listeners/guildCreate.ts
@@ -13,16 +13,21 @@ export default class GuildCreate extends Listener {
   async exec(guild: FireGuild) {
     if (!this.client.guildSettings.items.has(guild.id))
       await this.client.guildSettings.init(guild.id);
-    await (this.client.getCommand("description") as Description)
-      .setDesc(
-        this.client.guilds.cache.get("564052798044504084") as FireGuild,
-        `Fire is an open-source, multi-purpose bot with ${this.client.commandHandler.modules.size} commands and is used in ${this.client.guilds.cache.size} servers.`
+    if (
+      (this.client.options.shards as number[]).includes(
+        this.client.util.getShard("564052798044504084")
       )
-      .catch(() =>
-        this.client.console.warn(
-          "[Listener] Failed to update description for Fire guild."
+    )
+      await (this.client.getCommand("description") as Description)
+        .setDesc(
+          this.client.guilds.cache.get("564052798044504084") as FireGuild,
+          `Fire is an open-source, multi-purpose bot with ${this.client.commandHandler.modules.size} commands and is used in ${this.client.guilds.cache.size} servers.`
         )
-      );
+        .catch(() =>
+          this.client.console.warn(
+            "[Listener] Failed to update description for Fire guild."
+          )
+        );
     this.client.console.log(
       `Fire joined a new guild! ${guild.name} (${guild.id}) with ${guild.memberCount} members`
     );
--- src/listeners/guildDelete.ts
+++ src/listeners/guildDelete.ts
@@ -11,16 +11,21 @@ export default class GuildDelete extends Listener {
   }
 
   async exec(guild: FireGuild) {
-    await (this.client.getCommand("description") as Description)
-      .setDesc(
-        this.client.guilds.cache.get("564052798044504084") as FireGuild,
-        `Fire is an open-source, multi-purpose bot with ${this.client.commandHandler.modules.size} commands and is used in ${this.client.guilds.cache.size} servers.`
+    if (
+      (this.client.options.shards as number[]).includes(
+        this.client.util.getShard("564052798044504084")
       )
-      .catch(() =>
-        this.client.console.warn(
-          "[Listener] Failed to update description for Fire guild."
+    )
+      await (this.client.getCommand("description") as Description)
+        .setDesc(
+          this.client.guilds.cache.get("564052798044504084") as FireGuild,
+          `Fire is an open-source, multi-purpose bot with ${this.client.commandHandler.modules.size} commands and is used in ${this.client.guilds.cache.size} servers.`
         )
-      );
+        .catch(() =>
+          this.client.console.warn(
+            "[Listener] Failed to update description for Fire guild."
+          )
+        );
     this.client.console.log(
       `Fire left a guild! ${guild.name} (${guild.id}) with ${guild.memberCount} members owned by ${guild.owner}`
     );
