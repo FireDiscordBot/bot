258428bbf30c5c487020704cde9b2b3a365c51ad
Jake Ward <geek@gaminggeek.dev>
2020-12-10 05:08:53 +0000

    send READY_CLIENT on RESTART_CLIENT

--- src/ws/events/RestartEvent.ts
+++ src/ws/events/RestartEvent.ts
@@ -1,5 +1,8 @@
+import { getAllCommands, getCommands } from "../../../lib/util/commandutil";
+import { MessageUtil } from "../../../lib/ws/util/MessageUtil";
 import { EventType } from "../../../lib/ws/util/constants";
 import { Event } from "../../../lib/ws/event/Event";
+import { Message } from "../../../lib/ws/Message";
 import { Manager } from "../../../lib/Manager";
 
 export default class RestartEvent extends Event {
@@ -15,8 +18,21 @@ export default class RestartEvent extends Event {
     if (
       currentOptions.shardCount == data.shardCount &&
       JSON.stringify(currentOptions.shards) == JSON.stringify(data.shards)
-    )
+    ) {
+      this.manager.client.manager.ws?.send(
+        MessageUtil.encode(
+          new Message(EventType.READY_CLIENT, {
+            id: this.manager.client.manager.id,
+            commands: getCommands(this.manager.client),
+            allCommands: getAllCommands(this.manager.client),
+            avatar: this.manager.client.user.displayAvatarURL({
+              size: 4096,
+            }),
+          })
+        )
+      );
       return;
+    }
     this.manager.relaunch(data || { shardCount: 1, shards: [0] });
   }
 }
