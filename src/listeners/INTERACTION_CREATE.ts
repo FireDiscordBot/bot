f64db4e68f7709b91b61b2fa603c8ee2095c4839
Jake Ward <geek@gaminggeek.dev>
2020-12-12 01:43:05 +0000

    add more data to error

--- src/listeners/INTERACTION_CREATE.ts
+++ src/listeners/INTERACTION_CREATE.ts
@@ -18,6 +18,7 @@ export default class InteractionCreate extends Listener {
   }
 
   async exec(command: SlashCommand) {
+    let hasAcked: boolean;
     try {
       const message = new SlashCommandMessage(this.client, command);
       if (!message.command) {
@@ -31,31 +32,18 @@ export default class InteractionCreate extends Listener {
           this.client.config.inviteLink
         );
       await message.generateContent();
-      if (!message.command.ephemeral) await message.channel.ack(true);
+      if (!message.command.ephemeral)
+        hasAcked = await message.channel.ack(true);
       // @ts-ignore
       const handled = await this.client.commandHandler.handle(message);
       if (typeof handled == "boolean" && !handled)
         return await message.error("SLASH_COMMAND_HANDLE_FAIL");
     } catch (error) {
       const guild = this.client.guilds.cache.get(command.guild_id);
-      if (!guild) {
-        // @ts-ignore
-        await this.client.api
-          // @ts-ignore
-          .interactions(this.id)(this.token)
-          .callback.post({
-            data: {
-              // @ts-ignore
-              type: 3,
-              data: {
-                content: `${emojis.error} An error occured while trying to handle this command that may be caused by the bot not being present
-
-Try inviting the bot (<${this.client.config.inviteLink}>) and try again.`,
-              },
-            },
-          })
-          .catch(() => {});
-      }
+      if (!guild)
+        hasAcked
+          ? await this.webhookError(command, error).catch(() => {})
+          : await this.callbackError(command, error).catch(() => {});
       if (typeof this.client.sentry !== "undefined") {
         const sentry = this.client.sentry;
         sentry.setExtras({
@@ -72,5 +60,44 @@ Try inviting the bot (<${this.client.config.inviteLink}>) and try again.`,
         });
       }
     }
+  }
+
+  async callbackError(command: SlashCommand, error: Error) {
+    // @ts-ignore
+    await this.client.api
+      // @ts-ignore
+      .interactions(command.id)(command.token)
+      .callback.post({
+        data: {
+          // @ts-ignore
+          type: 3,
+          data: {
+            content: `${emojis.error} An error occured while trying to handle this command that may be caused by the bot not being present...
+
+Try inviting the bot (<${this.client.config.inviteLink}>) and try again.
+
+Error Message: ${error.message}`,
+            flags: 64,
+          },
+        },
+      })
+      .catch(() => {});
+  }
+
+  async webhookError(command: SlashCommand, error: Error) {
+    // @ts-ignore
+    await this.client.api
+      // @ts-ignore
+      .webhooks(this.client.user.id)(command.token)
+      .post({
+        data: {
+          content: `${emojis.error} An error occured while trying to handle this command that may be caused by the bot not being present...
+
+Try inviting the bot (<${this.client.config.inviteLink}>) and try again.
+
+Error Message: ${error.message}`,
+        },
+      })
+      .catch(() => {});
   }
 }
