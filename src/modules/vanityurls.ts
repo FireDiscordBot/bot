5c772487a106003712e591bfb6be248d98aa5c58
Jake Ward <geek@gaminggeek.dev>
2021-01-16 14:44:04 +0000

    add VanityURLs#current, check blacklist in create, add auth

--- src/modules/vanityurls.ts
+++ src/modules/vanityurls.ts
@@ -3,13 +3,14 @@ import { Module } from "../../lib/util/module";
 import * as centra from "centra";
 import { FireGuild } from "../../lib/extensions/guild";
 import { Invite } from "discord.js";
+import { MessageEmbed } from "discord.js";
+import { Language } from "../../lib/util/language";
 
 export default class VanityURLs extends Module {
   blacklisted: string[];
 
   constructor() {
     super("vanityurls");
-    this.blacklisted = [];
   }
 
   async init() {
@@ -30,6 +31,7 @@ export default class VanityURLs extends Module {
       "PUT"
     )
       .header("User-Agent", "Fire Discord Bot")
+      .header("Authorization", process.env.VANITY_KEY)
       .body({ reason }, "json")
       .send();
     if (fetchReq.statusCode != 204)
@@ -45,6 +47,7 @@ export default class VanityURLs extends Module {
         : `https://inv.wtf/api/${code}`
     )
       .header("User-Agent", "Fire Discord Bot")
+      .header("Authorization", process.env.VANITY_KEY)
       .send();
     if (vanityReq.statusCode != 200) return false;
     const vanity = await vanityReq.json();
@@ -53,6 +56,7 @@ export default class VanityURLs extends Module {
   }
 
   async create(guild: FireGuild, code: string, invite: Invite) {
+    if (this.blacklisted.includes(guild.id)) return "blacklisted";
     code = code.toLowerCase();
     const currentResult = await this.client.db
       .query("SELECT code FROM vanity WHERE gid=$1;", [guild.id])
@@ -86,5 +90,40 @@ export default class VanityURLs extends Module {
       .first()
       .catch(() => {});
     if (deleteResult) this.client.emit("vanityDelete", original, deleteResult);
+  }
+
+  async current(guild: FireGuild, code: string, language?: Language) {
+    if (!language) language = guild.language;
+    const data = await this.getVanity(code);
+    if (typeof data == "boolean") return;
+    let splash: string;
+    if (guild.splash)
+      splash = guild
+        .splashURL({
+          size: 2048,
+          format: "png",
+        })
+        .replace("size=2048", "size=320");
+    else if (guild.discoverySplash)
+      splash = guild
+        .discoverySplashURL({
+          size: 2048,
+          format: "png",
+        })
+        .replace("size=2048", "size=320");
+    const embed = new MessageEmbed()
+      .setAuthor(
+        guild.name,
+        guild.iconURL({ size: 2048, format: "png", dynamic: true })
+      )
+      .setColor("#2ECC71")
+      .addField("URL", `https://inv.wtf/${code}`)
+      .setTimestamp(new Date());
+    if (guild.premium) {
+      embed.addField(language.get("CLICKS"), data.clicks);
+      embed.addField(language.get("LINKS"), data.links);
+    }
+    if (splash) embed.setImage(splash);
+    return embed;
   }
 }
