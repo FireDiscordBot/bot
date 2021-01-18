2b124dd8aa6f5e8262cd3b3e1b5e1c51300ae898
Jake Ward <geek@gaminggeek.dev>
2021-01-18 12:37:05 +0000

    cleaner code

--- src/commands/Main/help.ts
+++ src/commands/Main/help.ts
@@ -32,10 +32,8 @@ export default class Help extends Command {
 
   async sendHelp(message: FireMessage) {
     let fields: { name: string; value: string; inline: boolean }[] = [];
-    for (const name of this.client.commandHandler.categories.keyArray()) {
+    for (const [name, category] of this.client.commandHandler.categories) {
       if (name == "Admin" && !message.author.isSuperuser()) continue;
-      let category = this.client.commandHandler.categories.get(name);
-      if (!category) continue;
       let commands: string[] = [];
       category
         .filter((command) => {
