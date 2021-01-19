dfb5ee471eb26f3ba1a6a346ec4cb60b17497258
GamingGeek <geek@gaminggeek.dev>
2021-01-19 22:10:16 +0000

    remove description code & add prefix to log

--- src/listeners/guildCreate.ts
+++ src/listeners/guildCreate.ts
@@ -1,4 +1,3 @@
-import Description from "../commands/Configuration/desc";
 import { FireGuild } from "../../lib/extensions/guild";
 import { Listener } from "../../lib/util/listener";
 
@@ -20,24 +19,8 @@ export default class GuildCreate extends Listener {
     await guild.loadInviteRoles();
     await guild.loadPersistedRoles();
 
-    if (
-      (this.client.options.shards as number[]).includes(
-        this.client.util.getShard("564052798044504084")
-      )
-    )
-      await (this.client.getCommand("description") as Description)
-        .setDesc(
-          this.client.guilds.cache.get("564052798044504084") as FireGuild,
-          `Fire is an open-source, multi-purpose bot with ${this.client.commandHandler.modules.size} commands and is used in ${this.client.guilds.cache.size} servers.`
-        )
-        .catch(() =>
-          this.client.console.warn(
-            "[Listener] Failed to update description for Fire guild."
-          )
-        );
     this.client.console.log(
-      `Fire joined a new guild! ${guild.name} (${guild.id}) with ${guild.memberCount} members`
+      `[Guilds] Fire joined a new guild! ${guild.name} (${guild.id}) with ${guild.memberCount} members`
     );
-    // TODO Add botlist guild count posting
   }
 }
--- src/listeners/guildDelete.ts
+++ src/listeners/guildDelete.ts
@@ -1,4 +1,3 @@
-import Description from "../commands/Configuration/desc";
 import { FireGuild } from "../../lib/extensions/guild";
 import { Listener } from "../../lib/util/listener";
 
@@ -11,25 +10,9 @@ export default class GuildDelete extends Listener {
   }
 
   async exec(guild: FireGuild) {
-    if (
-      (this.client.options.shards as number[]).includes(
-        this.client.util.getShard("564052798044504084")
-      )
-    )
-      await (this.client.getCommand("description") as Description)
-        .setDesc(
-          this.client.guilds.cache.get("564052798044504084") as FireGuild,
-          `Fire is an open-source, multi-purpose bot with ${this.client.commandHandler.modules.size} commands and is used in ${this.client.guilds.cache.size} servers.`
-        )
-        .catch(() =>
-          this.client.console.warn(
-            "[Listener] Failed to update description for Fire guild."
-          )
-        );
     const owner = this.client.users.fetch(guild.ownerID, false);
     this.client.console.log(
-      `Fire left a guild! ${guild.name} (${guild.id}) with ${guild.memberCount} members owned by ${owner}`
+      `[Guilds] Fire left a guild! ${guild.name} (${guild.id}) with ${guild.memberCount} members owned by ${owner}`
     );
-    // TODO Add botlist guild count posting
   }
 }
