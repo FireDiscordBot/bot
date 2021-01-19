7896b17318a31b922fba0198d2a60f5c0cf59765
GamingGeek <geek@gaminggeek.dev>
2021-01-19 22:31:43 +0000

    update fire description event

---
+++ src/ws/events/UpdateFireDescriptionEvent.ts
@@ -0,0 +1,35 @@
+import Description from "../../commands/Configuration/desc";
+import { EventType } from "../../../lib/ws/util/constants";
+import { FireGuild } from "../../../lib/extensions/guild";
+import { Event } from "../../../lib/ws/event/Event";
+import { Manager } from "../../../lib/Manager";
+
+export default class UpdateFireDescriptionEvent extends Event {
+  constructor(manager: Manager) {
+    super(manager, EventType.UPDATE_FIRE_DESCRIPTION);
+  }
+
+  async run(data: { commands: number; count: number }) {
+    const { commands, count } = data;
+    const { client } = this.manager;
+
+    if (
+      !(client.options.shards as number[]).includes(
+        client.util.getShard(client.config.fireGuildId)
+      )
+    )
+      return;
+
+    const guild = client.guilds.cache.get(
+      client.config.fireGuildId
+    ) as FireGuild;
+    const command = client.getCommand("description") as Description;
+
+    await command
+      .setDesc(
+        guild,
+        `Fire is an open-source, multi-purpose bot with ${commands} commands in ${count} servers.`
+      )
+      .catch(() => {});
+  }
+}
