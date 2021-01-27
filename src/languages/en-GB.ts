a3cd9191cc38ebb0e89b7154d9ea6004f6effec2
Jake Ward <geek@gaminggeek.dev>
2021-01-27 19:11:56 +0000

    u

--- src/languages/en-GB.ts
+++ src/languages/en-GB.ts
@@ -1,7 +1,7 @@
 import { Language } from "../../lib/util/language";
 
 // This is a special language.
-// It copies en-US (except for LANGUAGE_COMMAND_HELLO) but
+// It copies en-US (except for a select few strings) but
 // will modify dates when using Date#toLocaleString
 export default class enGB extends Language {
   constructor() {
@@ -15,6 +15,12 @@ export default class enGB extends Language {
         type == "user"
           ? "Hello! You have successfully set Fire's language to English (GB) :D"
           : "Hello! You have successfully set Fire's language in this guild to English (GB). Want to set it just for you? Run the command in DMs";
+      this.language.COLOR_COMMAND_DESCRIPTION =
+        "Get information about a colour";
+      this.language.COLOR_ARGUMENT_INVALID = (random: string) =>
+        `That does not seem to be a valid colour, maybe try ${random}`;
+      this.language.COLOR_HEADING = (color: string) =>
+        `Information about the colour **${color}**`;
     }, 500);
   }
 }
