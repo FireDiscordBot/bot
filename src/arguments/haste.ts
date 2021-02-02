b094ccc74b0f4a0bface6d516310f395a417d5d5
Jake Ward <geek@gaminggeek.dev>
2021-02-02 00:31:47 +0000

    converter fixes

--- src/arguments/haste.ts
+++ src/arguments/haste.ts
@@ -12,27 +12,34 @@ export const hasteTypeCaster: ArgumentTypeCaster = async (
   message: FireMessage,
   phrase
 ): Promise<string> => {
-  const match = phrase.match(haste);
-  const domain = match.groups?.domain;
-  const key = match.groups?.key;
-  if (!domain) {
+  const match = haste.exec(phrase);
+  haste.lastIndex = 0;
+  const uploader = match?.groups?.uploader;
+  const key = match?.groups?.key;
+  if (!uploader) {
     await message.error("HASTE_INVALID_DOMAIN", supportedHaste.join(", "));
     return null;
   } else if (!key) {
     await message.error("HASTE_INVALID_URL");
   }
 
-  if (domain == "h.inv.wtf" && !message.author.isSuperuser()) {
+  if (uploader == "h.inv.wtf" && !message.author.isSuperuser()) {
     await message.error("HASTE_INVALID_DOMAIN", supportedHaste.join(", "));
     return null;
   }
 
-  const hasteReq = await centra(`https://${domain}/raw/${key}`)
+  const hasteReq = await centra(`https://${uploader}/raw/${key}`)
     .header("User-Agent", "Fire Discord Bot")
     .send()
     .catch(() => {});
   if (!hasteReq || hasteReq.statusCode != 200) {
     await message.error("HASTE_FETCH_FAILED");
     return null;
-  } else return hasteReq.body.toString();
+  } else {
+    const content = hasteReq.body?.toString();
+    if (!content) {
+      await message.error("HASTE_FETCH_FAILED");
+      return null;
+    } else return content;
+  }
 };
