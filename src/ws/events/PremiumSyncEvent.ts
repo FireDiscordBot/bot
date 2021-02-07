691993760184b8040d5a50a2e72b3c5f47b4241b
Jake Ward <geek@gaminggeek.dev>
2021-02-07 00:03:42 +0000

    give premium role to premium users

--- src/ws/events/PremiumSyncEvent.ts
+++ src/ws/events/PremiumSyncEvent.ts
@@ -7,14 +7,38 @@ export default class PremiumSyncEvent extends Event {
     super(manager, EventType.PREMIUM_SYNC);
   }
 
-  run(data: { guilds: string[]; user_id: string; action: "add" | "remove" }) {
+  async run(data: {
+    guilds: string[];
+    user_id: string;
+    action: "add" | "remove";
+  }) {
+    const { client } = this.manager;
+
     if (!data.guilds?.length) return;
-    this.manager.client.console.log(
+    client.console.log(
       `[Event] Received premium sync request for ${data.guilds.join(", ")}.`
     );
     for (const guild of data.guilds)
-      if (data.action == "remove")
-        this.manager.client.util.premium.delete(guild);
-      else this.manager.client.util.premium.set(guild, data.user_id);
+      if (data.action == "remove") client.util.premium.delete(guild);
+      else client.util.premium.set(guild, data.user_id);
+
+    // Premium role stuffs
+    if (
+      !(client.options.shards as number[]).includes(
+        client.util.getShard(client.config.fireGuildId)
+      )
+    )
+      return;
+
+    const userIds = [...client.util.premium.values()];
+    const guild = client.guilds.cache.get(client.config.fireGuildId);
+    const role = guild.roles.cache.get("564060922688176139");
+    const members = await guild.members.fetch().catch(() => {});
+    if (!members) return;
+    for (const [, member] of members)
+      if (member.roles.cache.has(role.id) && !userIds.includes(member.id))
+        await member.roles.remove(role, "premium is gone :crabrave:");
+      else if (userIds.includes(member.id))
+        await member.roles.add(role, "wow member now has premium");
   }
 }
