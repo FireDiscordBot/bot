e5cc419614243ce61def643ee89c3624085242bd
Jake Ward <geek@gaminggeek.dev>
2021-02-08 04:55:08 +0000

    adjust initial loading of premium, add role checks

--- src/inhibitors/premium.ts
+++ src/inhibitors/premium.ts
@@ -1,7 +1,11 @@
+import { SubscriptionStatus } from "../../lib/interfaces/premium";
 import { FireMessage } from "../../lib/extensions/message";
 import { Inhibitor } from "../../lib/util/inhibitor";
 import { Command } from "../../lib/util/command";
 import { Collection } from "discord.js";
+
+const paidStatuses = ["trialing", "active", "past_due"];
+const hasPaid = (status: SubscriptionStatus) => paidStatuses.includes(status);
 
 export default class PremiumInhibitor extends Inhibitor {
   constructor() {
@@ -21,10 +25,12 @@ export default class PremiumInhibitor extends Inhibitor {
     this.client.util.loadedData.premium = false;
     const premium = await this.client.db.query("SELECT * FROM premium;");
     for await (const row of premium) {
-      this.client.util.premium.set(
-        row.get("gid") as string,
-        row.get("uid") as string
-      );
+      this.client.util.premium.set(row.get("gid") as string, {
+        user: row.get("uid") as string,
+        periodEnd: 3133641600, // nice
+        status: "active",
+        limit: 1, // could be more but likely not
+      });
     }
     const premiumStripe = await this.client.db.query(
       "SELECT * FROM premium_stripe"
@@ -35,11 +41,42 @@ export default class PremiumInhibitor extends Inhibitor {
       const expiry = new Date((row.get("periodend") as number) * 1000);
       if (now > expiry) continue;
       for (const guild of guilds)
-        this.client.util.premium.set(guild, row.get("uid") as string);
+        this.client.util.premium.set(guild, {
+          status: row.get("status") as SubscriptionStatus,
+          limit: row.get("serverlimit") as 1 | 3 | 5,
+          user: row.get("uid") as string,
+          periodEnd: +expiry,
+        });
     }
     this.client.util.loadedData.premium = true;
     this.client.console.log(
       `[Premium] Successfully loaded ${this.client.util.premium.size} premium guilds`
     );
+
+    if (
+      !(this.client.options.shards as number[]).includes(
+        this.client.util.getShard(this.client.config.fireGuildId)
+      )
+    )
+      return;
+
+    let paidIds: string[] = [];
+    let removeIds: string[] = [];
+    for await (const entry of premiumStripe) {
+      const uid = entry.get("uid") as string;
+      const status = entry.get("status") as SubscriptionStatus;
+      if (uid && hasPaid(status)) paidIds.push(uid);
+      else if (uid) removeIds.push(uid);
+    }
+
+    const guild = this.client.guilds.cache.get(this.client.config.fireGuildId);
+    const role = guild.roles.cache.get("564060922688176139");
+    const members = await guild.members.fetch().catch(() => {});
+    if (!members) return;
+    for (const [, member] of members)
+      if (member.roles.cache.has(role.id) && removeIds.includes(member.id))
+        await member.roles.remove(role, "premium is gone :crabrave:");
+      else if (paidIds.includes(member.id))
+        await member.roles.add(role, "wow member now has premium");
   }
 }
