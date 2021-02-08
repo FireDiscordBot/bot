01df5cc0c1671033b9acd6e6609d5b10706beddf
Jake Ward <geek@gaminggeek.dev>
2021-02-08 04:55:15 +0000

    modify premium sync event

--- src/ws/events/PremiumSyncEvent.ts
+++ src/ws/events/PremiumSyncEvent.ts
@@ -1,6 +1,13 @@
+import {
+  PremiumData,
+  SubscriptionStatus,
+} from "../../../lib/interfaces/premium";
 import { EventType } from "../../../lib/ws/util/constants";
 import { Event } from "../../../lib/ws/event/Event";
 import { Manager } from "../../../lib/Manager";
+
+const paidStatuses = ["trialing", "active", "past_due"];
+const hasPaid = (status: SubscriptionStatus) => paidStatuses.includes(status);
 
 export default class PremiumSyncEvent extends Event {
   constructor(manager: Manager) {
@@ -8,14 +15,18 @@ export default class PremiumSyncEvent extends Event {
   }
 
   async run(data: {
-    guilds: string[];
-    user_id: string;
-    action: "add" | "remove";
+    [guild: string]: PremiumData & { action: "add" | "remove" };
   }) {
     const { client } = this.manager;
-    for (const guild of data.guilds)
-      if (data.action == "remove") client.util.premium.delete(guild);
-      else client.util.premium.set(guild, data.user_id);
+    for (const [guild, premium] of Object.entries(data))
+      if (premium.action == "remove") client.util.premium.delete(guild);
+      else
+        client.util.premium.set(guild, {
+          periodEnd: premium.periodEnd,
+          status: premium.status,
+          limit: premium.limit,
+          user: premium.user,
+        });
 
     // Premium role stuffs
     if (
@@ -25,24 +36,28 @@ export default class PremiumSyncEvent extends Event {
     )
       return;
 
-    let userIds: string[] = [];
     const premiumStripeResult = await client.db
       .query("SELECT uid FROM premium_stripe;")
       .catch(() => {});
     if (!premiumStripeResult) return;
+
+    let paidIds: string[] = [];
+    let removeIds: string[] = [];
     for await (const entry of premiumStripeResult) {
       const uid = entry.get("uid") as string;
-      if (uid) userIds.push(uid);
+      const status = entry.get("status") as SubscriptionStatus;
+      if (uid && hasPaid(status)) paidIds.push(uid);
+      else if (uid) removeIds.push(uid);
     }
 
     const guild = client.guilds.cache.get(client.config.fireGuildId);
     const role = guild.roles.cache.get("564060922688176139");
     const members = await guild.members.fetch().catch(() => {});
     if (!members) return;
-    // if (member.roles.cache.has(role.id) && !userIds.includes(member.id))
-    //   await member.roles.remove(role, "premium is gone :crabrave:");
     for (const [, member] of members)
-      if (userIds.includes(member.id))
+      if (member.roles.cache.has(role.id) && removeIds.includes(member.id))
+        await member.roles.remove(role, "premium is gone :crabrave:");
+      else if (paidIds.includes(member.id))
         await member.roles.add(role, "wow member now has premium");
   }
 }
