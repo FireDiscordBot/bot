3e94382251b0e80961861f185d04cb39bb0e2ef2
Jake Ward <geek@gaminggeek.dev>
2021-02-10 09:33:23 +0000

    bam

--- src/commands/Moderation/ban.ts
+++ src/commands/Moderation/ban.ts
@@ -33,7 +33,16 @@ export default class Ban extends Command {
           match: "rest",
         },
       ],
-      aliases: ["banish", "begone", "gtfo", "410", "perish", "bonk", "bean"],
+      aliases: [
+        "banish",
+        "begone",
+        "gtfo",
+        "410",
+        "perish",
+        "bonk",
+        "bean",
+        "bam",
+      ],
       restrictTo: "guild",
       moderatorOnly: true,
     });
