afa329cb5a75f02fee35dda649d60cd93b837ced
Jake Ward <geek@gaminggeek.dev>
2021-02-10 08:59:16 +0000

    bots do not deserve a farewell

--- src/listeners/guildMemberRemove.ts
+++ src/listeners/guildMemberRemove.ts
@@ -71,28 +71,30 @@ export default class GuildMemberRemove extends Listener {
 
     const language = member.guild.language;
 
-    let leaveMessage = member.guild.settings.get("greet.leavemsg") as string;
-    const channel = member.guild.channels.cache.get(
-      member.guild.settings.get("greet.leavechannel")
-    );
-    if (leaveMessage && channel instanceof TextChannel) {
-      const regexes = [
-        [joinleavemsgs.user, member.toString()],
-        [joinleavemsgs.mention, member.toMention()],
-        [joinleavemsgs.name, member.user.username],
-        [joinleavemsgs.discrim, member.user.discriminator],
-        [joinleavemsgs.guild, member.guild.name],
-        [
-          joinleavemsgs.count,
-          member.guild.memberCount.toLocaleString(member.guild.language.id),
-        ],
-      ];
-      for (const [regex, replacement] of regexes)
-        leaveMessage = leaveMessage.replace(
-          regex as RegExp,
-          replacement as string
-        );
-      await channel.send(leaveMessage).catch(() => {});
+    if (!member.user.bot) {
+      let leaveMessage = member.guild.settings.get("greet.leavemsg") as string;
+      const channel = member.guild.channels.cache.get(
+        member.guild.settings.get("greet.leavechannel")
+      );
+      if (leaveMessage && channel instanceof TextChannel) {
+        const regexes = [
+          [joinleavemsgs.user, member.toString()],
+          [joinleavemsgs.mention, member.toMention()],
+          [joinleavemsgs.name, member.user.username],
+          [joinleavemsgs.discrim, member.user.discriminator],
+          [joinleavemsgs.guild, member.guild.name],
+          [
+            joinleavemsgs.count,
+            member.guild.memberCount.toLocaleString(member.guild.language.id),
+          ],
+        ];
+        for (const [regex, replacement] of regexes)
+          leaveMessage = leaveMessage.replace(
+            regex as RegExp,
+            replacement as string
+          );
+        await channel.send(leaveMessage).catch(() => {});
+      }
     }
 
     if (member.guild.settings.has("log.members")) {
