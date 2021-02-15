789fad11ac86c4e39b2ee8c32b9f4eccf2fffc92
Jake Ward <geek@gaminggeek.dev>
2021-02-15 15:27:32 +0000

    check for ws_host rather than env

--- lib/ws/Websocket.ts
+++ lib/ws/Websocket.ts
@@ -12,9 +12,9 @@ export class Websocket extends Client {
 
   constructor(manager: Manager) {
     super(
-      process.env.NODE_ENV == "development"
-        ? `ws://127.0.0.1:${process.env.WS_PORT}`
-        : `wss://${process.env.WS_HOST}`,
+      process.env.WS_HOST
+        ? `wss://${process.env.WS_HOST}`
+        : `ws://127.0.0.1:${process.env.WS_PORT}`,
       {
         headers: {
           "User-Agent": "Fire Discord Bot",
