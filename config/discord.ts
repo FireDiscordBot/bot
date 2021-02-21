fc8005226baaa27302b561c47e2fc1b1b2250896
Jake Ward <geek@gaminggeek.dev>
2021-02-21 13:01:38 +0000

    intents is now in ClientOptions base rather than ClientOptions#ws
    this also removes fetchAllMembers & messageEditHistoryMaxSize as they no longer exist

--- config/discord.ts
+++ config/discord.ts
@@ -18,26 +18,22 @@ export const discord: ClientOptions = {
     users: [],
     roles: [],
   },
-  messageEditHistoryMaxSize: 1,
   messageCacheLifetime: 150,
   messageSweepInterval: 60,
   messageCacheMaxSize: 30,
-  fetchAllMembers: false,
   restSweepInterval: 30,
   partials: ["REACTION", "MESSAGE", "CHANNEL", "GUILD_MEMBER", "USER"],
-  ws: {
-    intents:
-      intents.GUILDS |
-      intents.GUILD_MEMBERS |
-      // intents.GUILD_PRESENCES |
-      intents.GUILD_VOICE_STATES |
-      intents.GUILD_BANS |
-      intents.GUILD_INVITES |
-      intents.GUILD_MESSAGES |
-      intents.GUILD_MESSAGE_REACTIONS |
-      intents.DIRECT_MESSAGES |
-      intents.GUILD_VOICE_STATES,
-  },
+  intents:
+    intents.GUILDS |
+    intents.GUILD_MEMBERS |
+    // intents.GUILD_PRESENCES |
+    intents.GUILD_VOICE_STATES |
+    intents.GUILD_BANS |
+    intents.GUILD_INVITES |
+    intents.GUILD_MESSAGES |
+    intents.GUILD_MESSAGE_REACTIONS |
+    intents.DIRECT_MESSAGES |
+    intents.GUILD_VOICE_STATES,
   presence: {
     activity: {
       name: "things load...",
