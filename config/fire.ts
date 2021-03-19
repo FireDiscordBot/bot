d27c60202abf8b7eeb96bb002a24451e6005f129
Jake Ward <geek@gaminggeek.dev>
2021-03-19 11:07:59 +0000

    use user/member#toString

--- config/fire.ts
+++ config/fire.ts
@@ -6,10 +6,8 @@ export const fire = {
   dev: process.env.NODE_ENV == "development",
   readyMessage: (client: Fire) => {
     client.console.log("-------------------------");
-    client.console.log(
-      `Bot: ${client?.user?.username}#${client?.user?.discriminator}`
-    );
-    client.console.log(`ID: ${client?.user?.id}`);
+    client.console.log(`Bot: ${client.user}`);
+    client.console.log(`ID: ${client.user?.id}`);
     client.console.log(`Guilds: ${client.guilds.cache.size.toLocaleString()}`);
     client.console.log(
       `Users: ${(client.guilds.cache.size >= 1
