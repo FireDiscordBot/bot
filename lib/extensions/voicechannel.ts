a9c413c16ed61085e23db869305373bf02bb99a0
Jake Ward <geek@gaminggeek.dev>
2021-03-31 21:33:53 +0100

    add video quality

--- lib/extensions/voicechannel.ts
+++ lib/extensions/voicechannel.ts
@@ -3,6 +3,7 @@ import { FireGuild } from "./guild";
 import { Fire } from "@fire/lib/Fire";
 
 export class FireVoiceChannel extends VoiceChannel {
+  videoQuality: 1 | 2; // 1 == auto, 2 == 720p
   guild: FireGuild;
   region?: string;
   client: Fire;
@@ -17,6 +18,7 @@ export class FireVoiceChannel extends VoiceChannel {
     // @ts-ignore
     super._patch(data);
     this.region = data?.rtc_region;
+    this.videoQuality = data?.video_quality_mode;
   }
 }
 
