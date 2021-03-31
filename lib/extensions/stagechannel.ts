92ab6f3ea6dc86b34edc8b94a56caed4989928ba
Jake Ward <geek@gaminggeek.dev>
2021-03-31 21:33:47 +0100

    extend stage

---
+++ lib/extensions/stagechannel.ts
@@ -0,0 +1,25 @@
+import { StageChannel, Structures } from "discord.js";
+import { FireGuild } from "./guild";
+import { Fire } from "@fire/lib/Fire";
+
+export class FireStageChannel extends StageChannel {
+  videoQuality: 1 | 2; // 1 == auto, 2 == 720p
+  guild: FireGuild;
+  region?: string;
+  client: Fire;
+
+  constructor(guild: FireGuild, data?: object) {
+    super(guild, data);
+    // @ts-ignore
+    this.region = data.rtc_region;
+  }
+
+  _patch(data: any) {
+    // @ts-ignore
+    super._patch(data);
+    this.region = data?.rtc_region;
+    this.videoQuality = data?.video_quality_mode;
+  }
+}
+
+Structures.extend("StageChannel", () => FireStageChannel);
