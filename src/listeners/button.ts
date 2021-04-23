640f50823eaa7f1ea408d3f4fb10ae6e55765de0
Jake Ward <geek@gaminggeek.dev>
2021-04-23 17:33:16 +0100

    add check for owner

--- src/listeners/button.ts
+++ src/listeners/button.ts
@@ -15,25 +15,25 @@ export default class Button extends Listener {
   }
 
   // used to handle generic buttons, like ticket close or reaction roles
-  async exec(message: ButtonMessage) {
+  async exec(button: ButtonMessage) {
     // Run handlers
     try {
-      if (this.client.buttonHandlers.has(message.custom_id))
-        this.client.buttonHandlers.get(message.custom_id)(message);
+      if (this.client.buttonHandlers.has(button.custom_id))
+        this.client.buttonHandlers.get(button.custom_id)(button);
     } catch {}
     try {
-      if (this.client.buttonHandlersOnce.has(message.custom_id)) {
-        const handler = this.client.buttonHandlersOnce.get(message.custom_id);
-        this.client.buttonHandlersOnce.delete(message.custom_id);
-        handler(message);
+      if (this.client.buttonHandlersOnce.has(button.custom_id)) {
+        const handler = this.client.buttonHandlersOnce.get(button.custom_id);
+        this.client.buttonHandlersOnce.delete(button.custom_id);
+        handler(button);
       }
     } catch {}
 
     // handle ticket close buttons
-    if (message.custom_id.startsWith("ticket_close")) {
-      const { guild } = message;
+    if (button.custom_id.startsWith("ticket_close")) {
+      const { guild } = button;
       if (!guild) return;
-      const channelId = message.custom_id.slice(13);
+      const channelId = button.custom_id.slice(13);
       const channel = this.client.channels.cache.get(
         channelId
       ) as FireTextChannel;
@@ -42,46 +42,48 @@ export default class Button extends Listener {
         const closure = await guild
           .closeTicket(
             channel,
-            message.member,
+            button.member,
             guild.language.get("TICKET_CLOSE_BUTTON") as string
           )
           .catch(() => {});
         if (closure == "forbidden")
-          return await message.error("TICKET_CLOSE_FORBIDDEN");
+          return await button.error("TICKET_CLOSE_FORBIDDEN");
         else if (closure == "nonticket")
-          return await message.error("TICKET_NON_TICKET");
+          return await button.error("TICKET_NON_TICKET");
       } else return;
     }
 
-    if (message.custom_id.startsWith("sk1er_support_")) {
-      const type = message.custom_id.slice(14);
+    if (button.custom_id.startsWith("sk1er_support_")) {
+      const type = button.custom_id.slice(14);
       if (!type || !validSk1erTypes.includes(type)) return;
       const sk1erModule = this.client.getModule("sk1er") as Sk1er;
       if (!sk1erModule) return;
 
-      if (sk1erModule.ticketConfirm.includes(`${message.author.id}_${type}`)) {
+      if (sk1erModule.ticketConfirm.includes(`${button.author.id}_${type}`)) {
         sk1erModule.ticketConfirm = sk1erModule.ticketConfirm.filter(
-          (id) => id != `${message.author.id}_${type}`
+          (id) => id != `${button.author.id}_${type}`
         );
         const ticket = await sk1erModule
-          .handleSupport(message, message.author)
+          .handleSupport(button, button.author)
           .catch((e: Error) => e);
         if (!(ticket instanceof FireTextChannel))
           this.client.console.error(
-            `[Sk1er] Failed to make ticket for ${message.author} due to ${ticket}`
+            `[Sk1er] Failed to make ticket for ${button.author} due to ${ticket}`
           );
         return;
       } else {
-        sk1erModule.ticketConfirm.push(`${message.author.id}_${type}`);
-        message.flags += 64; // set ephemeral
-        return await message.error("SK1ER_SUPPORT_CONFIRM");
+        sk1erModule.ticketConfirm.push(`${button.author.id}_${type}`);
+        button.flags += 64; // set ephemeral
+        return await button.error("SK1ER_SUPPORT_CONFIRM");
       }
     }
 
     if (
-      validPaginatorIds.includes(message.custom_id) &&
-      message.message?.paginator
+      validPaginatorIds.includes(button.custom_id) &&
+      button.message?.paginator &&
+      button.message.paginator.ready &&
+      button.message.paginator.owner?.id == button.author.id
     )
-      await message.message?.paginator.buttonHandler(message).catch(() => {});
+      await button.message?.paginator.buttonHandler(button).catch(() => {});
   }
 }
