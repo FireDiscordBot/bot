5ade72967c21594e3f4754eba5549001e61e7147
Jake Ward <geek@gaminggeek.dev>
2021-05-05 21:33:58 +0100

    update sk1er support strings

--- src/languages/en-US.ts
+++ src/languages/en-US.ts
@@ -1296,10 +1296,18 @@ ${success.map((s) => "- " + s).join("\n")}${
           )}`,
         SK1ER_BETA_MOVED:
           "Beta testing for Sk1er LLC mods has been moved to our support server! You can join with discord.gg/d4KFR9H",
-        SK1ER_SUPPORT_CONFIRM:
-          "Please ensure you have all necessary details ready to give in the ticket. If you **do not** want to open a ticket, you can click the red button below. Otherwise, in a few seconds you will be able to click the green button",
-        SK1ER_SUPPORT_CONFIRM_EDIT:
-          "Please ensure you have all necessary details ready to give in the ticket. If you **do not** want to open a ticket, you can click the red button below. Otherwise, click the green button to open the ticket",
+        SK1ER_SUPPORT_CONFIRM: `Please ensure you have all necessary details ready to give in the ticket. If you **do not** want to open a ticket, you can click the red button below. Otherwise, in a few seconds you will be able to click the green button
+          
+Do note that we do not provide support for the following:
+* Non-Sk1er LLC mods
+* Hyperium
+* Users who have not purchased the game`,
+        SK1ER_SUPPORT_CONFIRM_EDIT: `Please ensure you have all necessary details ready to give in the ticket. If you **do not** want to open a ticket, you can click the red button below. Otherwise, click the green button to open the ticket
+
+Do note that we do not provide support for the following:
+* Non-Sk1er LLC mods
+* Hyperium
+* Users who have not purchased the game`,
         SK1ER_SUPPORT_CANCELLED:
           'Alright, you can go ahead and click "Dismiss Message"',
         SK1ER_SUPPORT_FAIL: (reason: string) =>
