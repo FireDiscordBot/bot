8f60f89f78dffdc9688da5b4affc99342d2d20ff
Jake Ward <geek@gaminggeek.dev>
2021-05-07 18:34:07 +0100

    modify ecosystem file

--- fire.config.js
+++ fire.config.js
@@ -67,5 +67,20 @@ module.exports = {
       exp_backoff_restart_delay: 2500,
       // node_args: "--expose-gc",
     },
+    {
+      name: "fire-beta",
+      script: "dist/src/index.js",
+      exec_mode: "cluster",
+      instances: 1,
+      wait_ready: true,
+      listen_timeout: 20000,
+      automation: false,
+      env: {
+        NODE_ENV: "development",
+      },
+      max_memory_restart: "1G",
+      exp_backoff_restart_delay: 2500,
+      // node_args: "--expose-gc",
+    },
   ],
 };
