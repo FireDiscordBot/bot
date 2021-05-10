a7af2a143e0039bc7dc67492035b30a87c31f951
Jake Ward <geek@gaminggeek.dev>
2021-05-10 15:59:43 +0100

    support embeds in forward message

--- src/ws/events/ForwardMessageEvent.ts
+++ src/ws/events/ForwardMessageEvent.ts
@@ -1,7 +1,9 @@
 import { FireTextChannel } from "@fire/lib/extensions/textchannel";
 import { EventType } from "@fire/lib/ws/util/constants";
 import { Event } from "@fire/lib/ws/event/Event";
+import { APIEmbed } from "discord-api-types";
 import { Manager } from "@fire/lib/Manager";
+import { MessageEmbed } from "discord.js";
 
 export default class ForwardMessageEvent extends Event {
   constructor(manager: Manager) {
@@ -9,24 +11,38 @@ export default class ForwardMessageEvent extends Event {
   }
 
   async run(data: {
+    message: string | APIEmbed;
     parseUsers?: string[];
-    message: string;
     channel?: string;
   }) {
     if (!this.manager.client.channels.cache.has(data.channel)) return;
 
-    let content = data.message;
+    let content = typeof data.message == "string" ? data.message : null;
+    let embed =
+      typeof data.message == "object" ? JSON.stringify(data.message) : null;
     for (const id of data.parseUsers) {
       const user = await this.manager.client.users.fetch(id).catch(() => {});
       if (user)
-        content = content.replace(
+        content = (content ?? embed).replace(
           new RegExp(id, "gim"),
           `${user} (${user.id})`
         );
     }
+
+    if (embed) {
+      try {
+        new MessageEmbed(JSON.parse(embed));
+      } catch {
+        embed = null;
+      }
+    }
+
     const channel = this.manager.client.channels.cache.get(
       data.channel
     ) as FireTextChannel;
-    if (channel.type == "text") await channel.send(content).catch(() => {});
+    if (channel.type == "text")
+      await channel
+        .send(content, { embed: new MessageEmbed(JSON.parse(embed)) })
+        .catch(() => {});
   }
 }
