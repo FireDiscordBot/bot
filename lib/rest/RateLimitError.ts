9b9eb637c06a33e8c9910d42db0afbf526359534
Jake Ward <geek@gaminggeek.dev>
2021-06-11 20:11:01 +0100

    bring rest code up to master

---
+++ lib/rest/RateLimitError.ts
@@ -0,0 +1,24 @@
+/**
+ * Represents a RateLimit error from a request.
+ * @extends Error
+ */
+export class RateLimitError extends Error {
+  timeout: number;
+  method: string;
+  path: string;
+  route: string;
+  global: boolean;
+  limit: number;
+
+  constructor({ timeout, limit, method, path, route, global }) {
+    super(`A ${global ? 'global ' : ''}rate limit was hit on route ${route}`);
+
+    this.name = 'RateLimitError';
+    this.timeout = timeout;
+    this.method = method;
+    this.path = path;
+    this.route = route;
+    this.global = global;
+    this.limit = limit;
+  }
+}
