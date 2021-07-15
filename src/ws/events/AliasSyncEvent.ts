04cd2962d12768b065ad00796460fd1dba3b7472
Jake Ward <geek@gaminggeek.dev>
2021-07-15 15:20:46 +0100

    add dupe checks for aliases and sync

---
+++ src/ws/events/AliasSyncEvent.ts
@@ -0,0 +1,14 @@
+import { EventType } from "@fire/lib/ws/util/constants";
+import { Event } from "@fire/lib/ws/event/Event";
+import { Manager } from "@fire/lib/Manager";
+import { Snowflake } from "discord.js";
+
+export default class AliasSyncEvent extends Event {
+  constructor(manager: Manager) {
+    super(manager, EventType.ALIAS_SYNC);
+  }
+
+  run(data: { user: Snowflake; aliases: string[] }) {
+    this.manager.client.aliases.set(data.user, data.aliases);
+  }
+}
