be7c89c563f701b31f54267984c40ae5221348d3
Jake Ward <geek@gaminggeek.dev>
2021-07-26 22:40:30 +0100

    context menu reminders

--- src/commands/Utilities/remind.ts
+++ src/commands/Utilities/remind.ts
@@ -1,11 +1,29 @@
+import { SlashCommandMessage } from "@fire/lib/extensions/slashcommandmessage";
+import ReminderSendEvent from "@fire/src/ws/events/ReminderSendEvent";
+import {
+  MessageActionRow,
+  MessageButton,
+  MessageSelectMenu,
+  SnowflakeUtil,
+} from "discord.js";
+import { Language, LanguageKeys } from "@fire/lib/util/language";
 import { humanize, parseTime } from "@fire/lib/util/constants";
 import { FireMessage } from "@fire/lib/extensions/message";
-import { Language } from "@fire/lib/util/language";
+import { EventType } from "@fire/lib/ws/util/constants";
 import { Command } from "@fire/lib/util/command";
 import * as moment from "moment";
 
 const repeatRegex = /--repeat (\d*)/gim;
 const stepRegex = /--step ([^-]*)/gim;
+const reminderContextTimes = {
+  REMINDER_SNOOZE_FIVEMIN: "300000",
+  REMINDER_SNOOZE_HALFHOUR: "1800000",
+  REMINDER_SNOOZE_HOUR: "3600000",
+  REMINDER_SNOOZE_SIXHOURS: "21600000",
+  REMINDER_SNOOZE_HALFDAY: "43200000",
+  REMINDER_SNOOZE_DAY: "86400000",
+  REMINDER_SNOOZE_WEEK: "604800000",
+};
 
 export default class Remind extends Command {
   constructor() {
@@ -30,6 +48,69 @@ export default class Remind extends Command {
   }
 
   async exec(message: FireMessage, args: { reminder?: string }) {
+    // handle context menu before actual command
+    // context menu shenanigans
+    if (message instanceof SlashCommandMessage && message.isMessageContext()) {
+      const clickedMessage = (
+        message as SlashCommandMessage
+      ).slashCommand.options.getMessage("message", true);
+      if (!clickedMessage)
+        return await message.error("REMINDER_MISSING_CONTEXT");
+      const event = this.client.manager.eventHandler?.store?.get(
+        EventType.REMINDER_SEND
+      ) as ReminderSendEvent;
+      if (!event) return await message.error();
+      const now = +new Date();
+      // we push a dummy reminder that we use for "snoozing"
+      event.sent.push({
+        user: message.author.id,
+        text: clickedMessage.content,
+        link: clickedMessage.url,
+        timestamp: now,
+      });
+      const dropdown = new MessageSelectMenu()
+        .setPlaceholder(
+          message.author.language.get("REMINDER_CONTEXT_PLACEHOLDER")
+        )
+        .setCustomId(`!snooze:${now}`)
+        .setMaxValues(1)
+        .setMinValues(1)
+        .addOptions(
+          Object.entries(reminderContextTimes).map(([key, time]) => {
+            return {
+              label: message.author.language.get(key as LanguageKeys),
+              value: time,
+            };
+          })
+        );
+      const cancelSnowflake = SnowflakeUtil.generate();
+      const cancelButton = new MessageButton()
+        .setEmoji("534174796938870792")
+        .setStyle("DANGER")
+        .setCustomId(`!${cancelSnowflake}`);
+      this.client.buttonHandlersOnce.set(cancelSnowflake, (b) => {
+        event.sent = event.sent.filter((r) => r.timestamp != now);
+        b.channel
+          .update({
+            content: b.language.get("INTERACTION_CANCELLED"),
+            components: [],
+          })
+          .catch(() => {});
+      });
+      return await message.channel.send({
+        content: message.author.language.get("REMINDER_CONTEXT_CONTENT", {
+          content:
+              clickedMessage.content.length >= 503
+                ? clickedMessage.content.slice(0, 500) + "..."
+                : clickedMessage.content,
+        }),
+        components: [
+          new MessageActionRow().addComponents(dropdown),
+          new MessageActionRow().addComponents(cancelButton),
+        ],
+      });
+    }
+
     if (!args.reminder) return await message.error("REMINDER_MISSING_ARG");
     let repeat: number, step: string;
     const repeatExec = repeatRegex.exec(args.reminder);
