ab5357ba6cb6f33ee43f109b443ae29a3e9318ee
Jake Ward <geek@gaminggeek.dev>
2021-08-12 17:05:12 +0100

    sort imports

--- lib/rest/APIRequest.ts
+++ lib/rest/APIRequest.ts
@@ -1,10 +1,10 @@
 import { AbortController } from "abort-controller";
 import * as FormData from "@discordjs/form-data";
+import { RequestOptions } from "./APIRouter";
 import { RESTManager } from "./RESTManager";
 import { Constants } from "discord.js";
-import * as centra from "centra";
 import { Fire } from "@fire/lib/Fire";
-import { RequestOptions } from "./APIRouter";
+import * as centra from "centra";
 
 const { UserAgent } = Constants;
 
