394d529f6a32b6ab7aed5a114adcf44e9c4211b5
Jake Ward <geek@gaminggeek.dev>
2021-08-17 19:16:42 +0100

    disable steal

--- src/commands/Fun/steal.ts
+++ src/commands/Fun/steal.ts
@@ -34,44 +34,46 @@ export default class Steal extends Command {
           required: false,
         },
       ],
-      enableSlashCommand: true,
+      enableSlashCommand: false, // due to it being disabled
       ephemeral: true,
+      hidden: true,
     });
   }
 
   async exec(message: FireMessage, args: { emoji: string; name?: string }) {
-    let emoji = args.emoji;
-    let name = args.name || "stolen_emoji";
-    if (!emoji) return await message.error("STEAL_NOTHING");
-    if (snowflakeRegex.test(emoji.toString())) {
-      snowflakeRegex.lastIndex = 0;
-      emoji = `https://cdn.discordapp.com/emojis/${emoji}`;
-      const format = await this.getFormat(emoji);
-      if (!format) return await message.error("STEAL_INVALID_EMOJI");
-      else emoji = `${emoji}.${format}`;
-    } else if (emojiRegex.test(emoji)) {
-      emojiRegex.lastIndex = 0;
-      const match = emojiRegex.exec(emoji);
-      emojiRegex.lastIndex = 0;
-      emoji = `https://cdn.discordapp.com/emojis/${match.groups.id}.${
-        match[0].startsWith("<a") ? "gif" : "png"
-      }`;
-      if (!args.name) name = match.groups.name;
-    } else if (
-      !/^https?:\/\/cdn\.discordapp\.com(\/emojis\/\d{15,21})\.\w{3,4}/im.test(
-        emoji
-      )
-    )
-      return await message.error("STEAL_INVALID_EMOJI");
-    let created;
-    try {
-      created = await message.guild.emojis.create(emoji, name);
-    } catch (e) {
-      return await message.error("STEAL_CAUGHT", {
-        code: e instanceof DiscordAPIError ? e.code : 0,
-      });
-    }
-    return await message.success("STEAL_STOLEN", { emoji: created.toString() });
+    return await message.error("STEAL_DISABLED");
+    // let emoji = args.emoji;
+    // let name = args.name || "stolen_emoji";
+    // if (!emoji) return await message.error("STEAL_NOTHING");
+    // if (snowflakeRegex.test(emoji.toString())) {
+    //   snowflakeRegex.lastIndex = 0;
+    //   emoji = `https://cdn.discordapp.com/emojis/${emoji}`;
+    //   const format = await this.getFormat(emoji);
+    //   if (!format) return await message.error("STEAL_INVALID_EMOJI");
+    //   else emoji = `${emoji}.${format}`;
+    // } else if (emojiRegex.test(emoji)) {
+    //   emojiRegex.lastIndex = 0;
+    //   const match = emojiRegex.exec(emoji);
+    //   emojiRegex.lastIndex = 0;
+    //   emoji = `https://cdn.discordapp.com/emojis/${match.groups.id}.${
+    //     match[0].startsWith("<a") ? "gif" : "png"
+    //   }`;
+    //   if (!args.name) name = match.groups.name;
+    // } else if (
+    //   !/^https?:\/\/cdn\.discordapp\.com(\/emojis\/\d{15,21})\.\w{3,4}/im.test(
+    //     emoji
+    //   )
+    // )
+    //   return await message.error("STEAL_INVALID_EMOJI");
+    // let created;
+    // try {
+    //   created = await message.guild.emojis.create(emoji, name);
+    // } catch (e) {
+    //   return await message.error("STEAL_CAUGHT", {
+    //     code: e instanceof DiscordAPIError ? e.code : 0,
+    //   });
+    // }
+    // return await message.success("STEAL_STOLEN", { emoji: created.toString() });
   }
 
   async getFormat(url: string) {
