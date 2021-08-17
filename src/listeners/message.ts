82f2812621aac626eb436ab99498591f9d559299
Jake Ward <geek@gaminggeek.dev>
2021-08-17 08:42:36 +0100

    🎣

--- src/listeners/message.ts
+++ src/listeners/message.ts
@@ -126,7 +126,13 @@ export default class Message extends Listener {
         (lowerContent.includes(".ru") &&
           (lowerContent.includes("tradeoffer") ||
             lowerContent.includes("partner") ||
-            lowerContent.includes("cs:go"))))
+            lowerContent.includes("cs:go"))) ||
+        (lowerContent.includes("nitro") &&
+          lowerContent.includes("steam") &&
+          lowerContent.includes("http")) ||
+        (lowerContent.includes("leaving") &&
+          lowerContent.includes("fucking") &&
+          lowerContent.includes("game")))
     ) {
       if (process.env.NODE_ENV == "development")
         return await message.reply("triggered steam/nitro phishing detection");
