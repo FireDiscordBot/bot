37e54805581df54f4133cb8bc744ad7e8f38ff5c
Jake Ward <geek@gaminggeek.dev>
2021-10-01 13:17:18 +0100

    Add chance for upsell when finished msg command

--- src/listeners/commandFinished.ts
+++ src/listeners/commandFinished.ts
@@ -12,6 +12,21 @@ export default class CommandFinished extends Listener {
 
   async exec(message: FireMessage | ApplicationCommandMessage) {
     if (message instanceof ApplicationCommandMessage) return;
+
+    const chance = this.client.util.randInt(0, 100);
+    if (
+      chance > 30 &&
+      chance < 50 &&
+      message.util?.parsed?.command?.id != "help"
+    ) {
+      const upsellEmbed = await this.client.util.getSlashUpsellEmbed(message);
+      if (upsellEmbed)
+        return await message.reply({
+          embeds: [upsellEmbed],
+          allowedMentions: { repliedUser: true },
+        });
+    }
+
     // member cache sweep ignores members with
     // an active command util so once the command
     // finishes, we can dispose of the command util
