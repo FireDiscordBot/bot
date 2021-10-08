9c74189efb430673fe5844fd609fa67cd9e29886
Lengo <28582329+yogurtsyum@users.noreply.github.com>
2021-10-07 20:07:16 -0500

    Add open in browser link to icon command

--- src/commands/Utilities/icon.ts
+++ src/commands/Utilities/icon.ts
@@ -1,6 +1,6 @@
 import { FireMember } from "@fire/lib/extensions/guildmember";
 import { FireMessage } from "@fire/lib/extensions/message";
-import { MessageEmbed, Permissions } from "discord.js";
+import { MessageEmbed, Permissions, MessageActionRow, MessageButton } from "discord.js";
 import { FireUser } from "@fire/lib/extensions/user";
 import { Language } from "@fire/lib/util/language";
 import { Command } from "@fire/lib/util/command";
@@ -24,20 +24,30 @@ export default class Icon extends Command {
     message: FireMessage,
     args: { user: FireMember | FireUser | null }
   ) {
+    const iconURL = message.guild?.iconURL({
+      size: 2048,
+      format: "png",
+      dynamic: true,
+    });
+
     const embed = new MessageEmbed()
       .setColor(message.member?.displayColor ?? "#FFFFFF")
       .setTimestamp()
       .setTitle(
         message.language.get("ICON_TITLE", { guild: message.guild.name })
       )
-      .setImage(
-        message.guild?.iconURL({
-          size: 2048,
-          format: "png",
-          dynamic: true,
-        })
-      );
+      .setImage(iconURL);
 
-    return await message.channel.send({ embeds: [embed] });
+    return await message.channel.send({
+      embeds: [embed],
+      components: [
+        new MessageActionRow().addComponents(
+          new MessageButton()
+            .setStyle("LINK")
+            .setLabel(message.language.get("OPEN_IN_BROWSER"))
+            .setURL(iconURL)
+        ),
+      ],
+    });
   }
 }
