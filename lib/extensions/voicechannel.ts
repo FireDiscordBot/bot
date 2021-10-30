b391f4d70fb2d434d9c927690b0abf80367016a7
Jake Ward <geek@gaminggeek.dev>
2021-10-30 01:30:52 +0100

    add send method

--- lib/extensions/voicechannel.ts
+++ lib/extensions/voicechannel.ts
@@ -1,7 +1,15 @@
-import { RawGuildChannelData } from "discord.js/typings/rawDataTypes";
-import { VoiceChannel, Structures, MessageManager } from "discord.js";
 import { Fire } from "@fire/lib/Fire";
+import { APIMessage } from "discord-api-types";
+import {
+  MessageManager,
+  MessageOptions,
+  MessagePayload,
+  Structures,
+  VoiceChannel,
+} from "discord.js";
+import { RawGuildChannelData } from "discord.js/typings/rawDataTypes";
 import { FireGuild } from "./guild";
+import { FireMessage } from "./message";
 
 export class FireVoiceChannel extends VoiceChannel {
   messages: MessageManager;
@@ -12,6 +20,31 @@ export class FireVoiceChannel extends VoiceChannel {
     super(guild, data);
     // @ts-ignore (remove when text in vc is fully supported)
     this.messages = new MessageManager(this);
+  }
+
+  async send(
+    options: string | MessagePayload | MessageOptions
+  ): Promise<FireMessage> {
+    let messagePayload: MessagePayload;
+
+    if (options instanceof MessagePayload) {
+      messagePayload = options.resolveData();
+    } else {
+      // @ts-ignore
+      messagePayload = MessagePayload.create(this, options).resolveData();
+    }
+
+    const { data, files } = await messagePayload.resolveFiles();
+    const d = await this.client.req.channels[this.id].messages.post<APIMessage>(
+      {
+        data,
+        files,
+      }
+    );
+
+    return (this.messages.cache.get(d.id) ??
+      // @ts-ignore
+      this.messages._add(d)) as FireMessage;
   }
 }
 
