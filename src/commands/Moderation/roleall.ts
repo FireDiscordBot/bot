7bb097d5c25f74904a44c29556aa76fd3068a0e8
EthanDevelops <66741050+EthanDevelops@users.noreply.github.com>
2021-11-14 20:00:02 +0000

    tried to fix some stuff

--- src/commands/Moderation/roleall.ts
+++ src/commands/Moderation/roleall.ts
@@ -3,26 +3,21 @@
 */
 import { FireMessage } from "@fire/lib/extensions/message";
 import { Command } from "@fire/lib/util/command";
-import 
-{ GuildMember, 
-  Role,
-  Permissions }  from 'discord.js';
+import { FireMember } from "@fire/lib/extensions/guildmember";
+import { Role, Permissions }  from 'discord.js';
 
 export default class RoleAllCommand extends Command {
-	public constructor() {
+	constructor() {
 		super('roleAll', {
-			aliases: ['role-all', 'rall'],
-			category: 'admin',
+			category: 'moderation',
             userPermissions: [Permissions.FLAGS.ADMINISTRATOR],
+			clientPermissions: [Permissions.FLAGS.MANAGE_ROLES],
 			description: "Give a role to al the members of this server",
 			args: [
 				{
 					id: 'role',
 					type: 'role',
-					prompt: {
-						start: 'What role would you like to give to all the members of this server?',
-						retry: '{error} Pick a valid role.'
-					}
+					required: true,
 				}
 			],
             enableSlashCommand: true,
@@ -33,10 +28,6 @@ export default class RoleAllCommand extends Command {
 	async exec(
         message: FireMessage , 
         args: { role: Role }) {
-		if (!message.guild) return await message.util.reply('This command can only be run in a server.');
-
-		if (!message.member!.permissions.has('ADMINISTRATOR') )
-			return await message.util.reply(`You must have admin permissions to use this command.`);
 
 		if (args.role.comparePositionTo(message.guild.me!.roles.highest) >= 0 && !args.role) {
 			return await message.util.reply('I cannot add a role higher or equal to my highest role.');
@@ -44,36 +35,25 @@ export default class RoleAllCommand extends Command {
 
 		let members = await message.guild.members.fetch();
 
-		members = members.filter((member: GuildMember) => {
-			try {
+		members = members.filter((member: FireMember) => {
 				if (member.user.bot) return false;
 				if (member.roles.cache.has(args.role.id)) return false;
-			} catch {
-				return false;
-			}
 			return true;
 		});
 
-		await message.util.reply(`Adding roles to ${members.size} members. This may take a while...`);
+		await message.channel.send(`Adding roles to ${members.size} members. This may take a while...`);
 
-		const promises = members.map((member: GuildMember) => {
-			return member.roles.add(args.role, `Role All Command - triggered by ${message.author.tag} (${message.author.id})`);
+		const promises = members.map((member: FireMember) => {
+			return member.roles.add(args.role, 'Role All Command');
 		});
 
 		const failed = (await Promise.allSettled(promises)).filter((val) => val.status === 'rejected');
 
 		if (!failed.length) {
-			await message.util.reply({
+			await message.channel.send({
 				content: `Finished adding <@&${args.role.id}> roles.${
 					members.size > 1 ? 's' : ''
 				}.`,
-			});
-		} else {
-			const array = [...members.values()];
-			await message.util.reply({
-				content: ` Finished adding the  <@&${args.role.id}> with errors.${
-					members.size - failed.length > 1 ? 's' : ''
-				}! Failed members:\n${failed.map((_, index) => `<@${array[index].id}>`).join(' ')}`,
 			});
 		}
 	}
