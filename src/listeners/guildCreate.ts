36ba587557e746dfc68ef72b50ae63fcb5650cd1
Jake Ward <geek@gaminggeek.dev>
2021-11-19 15:40:02 +0000

    send discovery update on guild create/delete

--- src/listeners/guildCreate.ts
+++ src/listeners/guildCreate.ts
@@ -1,5 +1,9 @@
 import { FireGuild } from "@fire/lib/extensions/guild";
+import { DiscoveryUpdateOp } from "@fire/lib/interfaces/stats";
 import { Listener } from "@fire/lib/util/listener";
+import { Message } from "@fire/lib/ws/Message";
+import { EventType } from "@fire/lib/ws/util/constants";
+import { MessageUtil } from "@fire/lib/ws/util/MessageUtil";
 
 export default class GuildCreate extends Listener {
   constructor() {
@@ -16,5 +20,15 @@ export default class GuildCreate extends Listener {
     this.client.console.log(
       `[Guilds] Fire joined a new guild! ${guild.name} (${guild.id}) with ${guild.memberCount} members`
     );
+
+    if (guild.isPublic() && this.client.manager.ws?.open)
+      this.client.manager.ws.send(
+        MessageUtil.encode(
+          new Message(EventType.DISCOVERY_UPDATE, {
+            op: DiscoveryUpdateOp.ADD,
+            guilds: [guild.getDiscoverableData()],
+          })
+        )
+      );
   }
 }
--- src/listeners/guildDelete.ts
+++ src/listeners/guildDelete.ts
@@ -1,6 +1,10 @@
 import { FireGuild } from "@fire/lib/extensions/guild";
 import { FireUser } from "@fire/lib/extensions/user";
+import { DiscoveryUpdateOp } from "@fire/lib/interfaces/stats";
 import { Listener } from "@fire/lib/util/listener";
+import { Message } from "@fire/lib/ws/Message";
+import { EventType } from "@fire/lib/ws/util/constants";
+import { MessageUtil } from "@fire/lib/ws/util/MessageUtil";
 
 export default class GuildDelete extends Listener {
   constructor() {
@@ -22,5 +26,15 @@ export default class GuildDelete extends Listener {
         guild.memberCount
       } members owned by ${owner ?? "an unknown user"} (${guild.ownerId})`
     );
+
+    if (this.client.manager.ws?.open)
+      this.client.manager.ws.send(
+        MessageUtil.encode(
+          new Message(EventType.DISCOVERY_UPDATE, {
+            op: DiscoveryUpdateOp.REMOVE,
+            guilds: [{ id: guild.id }],
+          })
+        )
+      );
   }
 }
