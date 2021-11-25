df01bc09ba73a763b5d061678a34f49f22815005
Jake Ward <geek@gaminggeek.dev>
2021-11-25 17:15:31 +0000

    change to parent if thread

--- src/commands/Moderation/block.ts
+++ src/commands/Moderation/block.ts
@@ -1,9 +1,10 @@
-import { FireTextChannel } from "@fire/lib/extensions/textchannel";
-import { Language, LanguageKeys } from "@fire/lib/util/language";
 import { FireMember } from "@fire/lib/extensions/guildmember";
 import { FireMessage } from "@fire/lib/extensions/message";
+import { FireTextChannel } from "@fire/lib/extensions/textchannel";
+import { FireVoiceChannel } from "@fire/lib/extensions/voicechannel";
 import { Command } from "@fire/lib/util/command";
-import { Permissions, Role } from "discord.js";
+import { Language, LanguageKeys } from "@fire/lib/util/language";
+import { NewsChannel, Permissions, Role, ThreadChannel } from "discord.js";
 
 export default class Block extends Command {
   constructor() {
@@ -54,6 +55,14 @@ export default class Block extends Command {
       args.toblock.rawPosition >= message.member?.roles?.highest?.rawPosition
     )
       return await message.error("BLOCK_ROLE_HIGH");
+
+    let channel = message.channel as
+      | FireTextChannel
+      | FireVoiceChannel
+      | NewsChannel
+      | ThreadChannel;
+    if (channel instanceof ThreadChannel)
+      channel = channel.parent as FireTextChannel | NewsChannel;
 
     await message.delete().catch(() => {});
     const blocked = await message.guild.block(
