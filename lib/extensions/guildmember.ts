1fb4049f176634982c59404cea0eae56be6abf87
Jake Ward <geek@gaminggeek.dev>
2021-12-02 00:12:19 +0000

    ignore admin for checking permission

--- lib/extensions/guildmember.ts
+++ lib/extensions/guildmember.ts
@@ -626,7 +626,7 @@ export class FireMember extends GuildMember {
       until &&
       until < +new Date() + 2419199999 &&
       this.guild.hasExperiment(1955682940, 1) &&
-      this.guild.me?.permissions?.has("MODERATE_MEMBERS");
+      this.guild.me?.permissions?.has("MODERATE_MEMBERS", false);
     if (!reason || !moderator) return "args";
     if (!moderator.isModerator(channel)) return "forbidden";
     if (!this.guild.muteRole && !canTimeOut) {
