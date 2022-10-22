cbe44fd44832bd8a9d6daae93c812559a505da90
Jake Ward <geek@gaminggeek.dev>
2022-10-22 21:06:14 +0100

    remove variables

--- lib/util/gitUtils.ts
+++ lib/util/gitUtils.ts
@@ -1,28 +1,21 @@
 import { execSync } from "child_process";
 import { readFileSync } from "fs";
 
-let currentHash: string, currentBranch: string;
-
 export const getCommitHash = () => {
-  if (currentHash) return currentHash;
   const rev = readFileSync(".git/HEAD")
     .toString()
     .trim()
     .split(/.*[: ]/)
     .slice(-1)[0];
   if (rev.indexOf("/") == -1) {
-    currentHash = rev;
-    return currentHash;
+    return rev;
   } else {
-    currentHash = readFileSync(".git/" + rev)
+    return readFileSync(".git/" + rev)
       .toString()
       .trim();
-    return currentHash;
   }
 };
 
 export const getBranch = () => {
-  if (currentBranch) return currentBranch;
-  currentBranch = execSync("git rev-parse --abbrev-ref HEAD").toString().trim();
-  return currentBranch;
+  return execSync("git rev-parse --abbrev-ref HEAD").toString().trim();
 };
