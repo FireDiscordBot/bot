a95cb138af4697c1a9123bad50a7b95564e23434
Jake Ward <geek@gaminggeek.dev>
2023-01-13 21:01:34 +0000

    Fix the fix

--- lib/extensions/guild.ts
+++ lib/extensions/guild.ts
@@ -1077,7 +1077,7 @@ export class FireGuild extends Guild {
       (this.channels.cache
         .filter((chan) => chan.type == "GUILD_CATEGORY")
         .get(
-          this.settings.get<Snowflake>("ticFix kets.parent")
+          this.settings.get<Snowflake>("tickets.parent")
         ) as CategoryChannel);
     if (!category) return "disabled";
     const limit = this.settings.get<number>("tickets.limit", 1);
