903d5d4ea5e61c739e6e36c756f3206860dbbbff
Jake Ward <geek@gaminggeek.dev>
2023-02-03 10:22:58 +0000

    Remove unnecessary comments

--- src/listeners/interaction.ts
+++ src/listeners/interaction.ts
@@ -332,13 +332,7 @@ export default class InteractionListener extends Listener {
         return await message.error("SLASH_COMMAND_BOT_REQUIRED", {
           invite: this.client.config.inviteLink,
         });
-      // // TODO: change to handleSlash and remove content
-      // await message.generateContent();
-      // // @ts-ignore
-      // await this.client.commandHandler.handle(message);
       await this.client.commandHandler.handleSlash(message);
-      // if (message.sent != "message")
-      //   await message.sourceMessage?.delete().catch(() => {});
     } catch (error) {
       const guild = this.client.guilds.cache.get(context.guildId);
       if (!guild)
