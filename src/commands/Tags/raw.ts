574827ccdc1a8e2f49539e14f3e47b71552ebfde
Jake Ward <geek@gaminggeek.dev>
2023-09-13 13:12:28 +0100

    Always create haste for tag raw

--- src/commands/Tags/raw.ts
+++ src/commands/Tags/raw.ts
@@ -5,8 +5,6 @@ import { FireMessage } from "@fire/lib/extensions/message";
 import { Language } from "@fire/lib/util/language";
 import { Command } from "@fire/lib/util/command";
 
-const markdownRegex = /[_\\~|\*`]/gim;
-
 export default class TagRaw extends Command {
   constructor() {
     super("tag-raw", {
@@ -57,12 +55,8 @@ export default class TagRaw extends Command {
     const manager = message.guild.tags;
     const cachedTag = await manager.getTag(tag);
     if (!cachedTag) return await message.error("TAG_INVALID_TAG", { tag });
-    const hasMarkdown = markdownRegex.test(cachedTag.content);
-    markdownRegex.lastIndex = 0;
-    if (hasMarkdown || cachedTag.content.length >= 2000)
-      return await message.channel.send({
-        content: await this.client.util.haste(cachedTag.content, false, "md"),
-      });
-    else return await message.channel.send({ content: cachedTag.content });
+    return await message.channel.send({
+      content: await this.client.util.haste(cachedTag.content, false, "md"),
+    });
   }
 }
