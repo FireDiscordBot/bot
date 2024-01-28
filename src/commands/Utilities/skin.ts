572318cb69a6bc5cccdacf784f6ceeea12a6e804
Jake Ward <geek@gaminggeek.dev>
2024-01-28 16:02:11 +0000

    oops I forgor to save the file

--- src/commands/Utilities/skin.ts
+++ src/commands/Utilities/skin.ts
@@ -1,5 +1,8 @@
 import { ApplicationCommandMessage } from "@fire/lib/extensions/appcommandmessage";
-import { ProfileNotFoundError } from "@fire/lib/util/clientutil";
+import {
+  MojangAPIError,
+  ProfileNotFoundError,
+} from "@fire/lib/util/clientutil";
 import { Command } from "@fire/lib/util/command";
 import { Language } from "@fire/lib/util/language";
 import { MessageEmbed } from "discord.js";
@@ -29,22 +32,31 @@ export default class Skin extends Command {
 
   async run(
     command: ApplicationCommandMessage,
-    args: { ign?: { match: RegExpMatchArray; matches: RegExpExecArray[] } }
+    args: { username?: { match: RegExpMatchArray; matches: RegExpExecArray[] } }
   ) {
-    if (!args.ign) return await command.error("MINECRAFT_INVALID_IGN");
-    const ign: string = args.ign.match[0];
-    let profile = await this.client.util.mcProfile(ign).catch(() => null);
+    if (!args.username) return await command.error("MINECRAFT_INVALID_IGN");
+    const ign: string = args.username.match[0];
+    let profile = await this.client.util
+      .mcProfile(ign)
+      .catch((e: MojangAPIError) => e);
     if (profile instanceof ProfileNotFoundError)
       return await command.error("MINECRAFT_PROFILE_FETCH_UNKNOWN");
+    else if (profile instanceof MojangAPIError)
+      return await command.error(
+        command.author.isSuperuser()
+          ? "MINECRAFT_PROFILE_FETCH_FAIL_FULL"
+          : "MINECRAFT_PROFILE_FETCH_FAIL_BASIC",
+        { ign, error: profile.message }
+      );
     const embed = new MessageEmbed()
       .setColor(command.member?.displayColor || "#FFFFFF")
       .setImage(
-        `https://visage.surgeplay.com/full/512/${profile}?ts=${+new Date()}`
+        `https://visage.surgeplay.com/full/512/${profile.id}?ts=${+new Date()}`
       )
-      .setFooter(
-        `Requested by ${command.author}`,
-        command.author.displayAvatarURL({ size: 2048, dynamic: true })
-      );
+      .setFooter({
+        text: `Requested by ${command.author}`,
+        iconURL: command.author.displayAvatarURL({ size: 2048, dynamic: true }),
+      });
     return await command.channel.send({ embeds: [embed] });
   }
 }
