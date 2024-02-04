6ab914debb90f5629d6086799d204b3328780299
Jake Ward <geek@gaminggeek.dev>
2024-02-04 14:26:19 +0000

    remove purchase support

--- src/modules/sk1er.ts
+++ src/modules/sk1er.ts
@@ -4,7 +4,12 @@ import { FireMember } from "@fire/lib/extensions/guildmember";
 import { FireMessage } from "@fire/lib/extensions/message";
 import { FireUser } from "@fire/lib/extensions/user";
 import { Module } from "@fire/lib/util/module";
-import { CategoryChannel, MessageReaction, Snowflake } from "discord.js";
+import { CategoryChannel, Snowflake } from "discord.js";
+
+const categories = {
+  "🖥️": "958837673651478529",
+  "🐛": "958837723534336000",
+};
 
 export default class Sk1er extends Module {
   guildId: Snowflake;
@@ -29,80 +34,43 @@ export default class Sk1er extends Module {
     }
   }
 
-  async handleSupport(
-    trigger: MessageReaction | ComponentMessage,
-    user: FireUser
-  ) {
+  async handleSupport(trigger: ComponentMessage, user: FireUser) {
     const member =
-      trigger instanceof ComponentMessage && trigger.member
-        ? trigger.member
-        : ((await this.guild.members.fetch(user)) as FireMember);
+      trigger.member ??
+      ((await this.guild.members.fetch(user).catch(() => {})) as FireMember);
     if (!member) return "no member"; // how
     let emoji: string;
-    if (trigger instanceof MessageReaction) {
-      emoji = trigger.emoji.name;
-      try {
-        await trigger.users.remove(user);
-      } catch {}
-    } else {
-      if (!trigger.message) return "no message";
-      const component = (trigger.message as FireMessage).components
-        .map((component) =>
-          component.type == "ACTION_ROW"
-            ? component?.components ?? component
-            : component
-        )
-        .flat()
-        .find(
-          (component) =>
-            component.type == "BUTTON" &&
-            component.style != "LINK" &&
-            component.customId == trigger.customId
-        );
-      if (component.type != "BUTTON" || component.style == "LINK")
-        return "non button";
-      if (!component.emoji) return "unknown emoji";
-      emoji =
-        typeof component.emoji == "string"
-          ? component.emoji
-          : component.emoji.name;
-    }
+    if (!trigger.message) return "no message";
+    const component = (trigger.message as FireMessage).components
+      .map((component) =>
+        component.type == "ACTION_ROW"
+          ? component?.components ?? component
+          : component
+      )
+      .flat()
+      .find(
+        (component) =>
+          component.type == "BUTTON" &&
+          component.style != "LINK" &&
+          component.customId == trigger.customId
+      );
+    if (component.type != "BUTTON" || component.style == "LINK")
+      return "non button";
+    if (!component.emoji) return "unknown emoji";
+    emoji =
+      typeof component.emoji == "string"
+        ? component.emoji
+        : component.emoji.name;
     if (!emoji) return "no emoji";
-    if (emoji == "🖥️") {
-      const category = this.guild.channels.cache.get(
-        "958837673651478529"
-      ) as CategoryChannel;
-      if (!category) return "no category";
-      return await this.guild.createTicket(
-        member,
-        "General Support",
-        null,
-        category
-      );
-    }
-    if (emoji == "💸") {
-      const category = this.guild.channels.cache.get(
-        "958827935253545020"
-      ) as CategoryChannel;
-      if (!category) return "no category";
-      return await this.guild.createTicket(
-        member,
-        "Purchase Support",
-        null,
-        category
-      );
-    }
-    if (emoji == "🐛") {
-      const category = this.guild.channels.cache.get(
-        "958837723534336000"
-      ) as CategoryChannel;
-      if (!category) return "no category";
-      return await this.guild.createTicket(
-        member,
-        "Bug Report",
-        null,
-        category
-      );
-    }
+    const category = this.guild.channels.cache.get(
+      categories[emoji]
+    ) as CategoryChannel;
+    if (!category) return "no category";
+    return await this.guild.createTicket(
+      member,
+      component.label,
+      null,
+      category
+    );
   }
 }
