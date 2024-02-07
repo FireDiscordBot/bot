43b1b112755aa03ec9e2407fc48e137427f7361e
Jake Ward <geek@gaminggeek.dev>
2024-02-07 19:25:10 +0000

    Update Forge deps regex

--- src/modules/mclogs.ts
+++ src/modules/mclogs.ts
@@ -215,7 +215,7 @@ export default class MCLogs extends Module {
       forgeValidModFile:
         /Found valid mod file (?<source>[\w\s\-\+\.'`!()\[\]]+\.jar) with {(?<modid>[a-z][a-z0-9_.-]{1,63}(?:,[a-z][a-z0-9_.-]{1,63})*)} mods - versions {(?<version>[\w\-\+\.!\[\]]+(?:,[\w\s\-\+\.!\[\]]+)*)}/gim,
       forgeDependenciesError:
-        /Mod ID: '(?<dep>[a-z][a-z0-9_.-]{1,63})', Requested by: '(?<requiredby>[a-z][a-z0-9_.-]{1,63})', Expected range: '\[(?<low>[\w\.\-+]+),(?<high>[\w\.\-+]+)?\)', Actual version: '(?<actual>[\w\.\-+]+|\[MISSING\])'/gim,
+        /Mod ID: '(?<dep>[a-z][a-z0-9_.-]{1,63})', Requested by: '(?<requiredby>[a-z][a-z0-9_.-]{1,63})', Expected range: '[\(\[](?<low>[\w\.\-+]+),(?<high>[\w\.\-+]+)?[\)\]]', Actual version: '(?<actual>[\w\.\-+]+|\[MISSING\])'/gim,
       fabricModsHeader: /\[main\/INFO]: Loading \d{1,4} mods:/gim,
       classicFabricModsEntry:
         /^\s+\- (?<modid>[a-z0-9_\-]{2,}) (?<version>[\w\.\-+]+)(?: via (?<via>[a-z0-9_\-]{2,}))?$/gim,
