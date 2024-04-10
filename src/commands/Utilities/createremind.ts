d1e49e0cfc2d8181070ca9d85b40e0a64f75775e
Jake Ward <geek@gaminggeek.dev>
2024-04-10 04:48:13 +0100

    [INSTALL] I wasted time

--- src/commands/Utilities/createremind.ts
+++ src/commands/Utilities/createremind.ts
@@ -62,50 +62,6 @@ const getContextOptions = (
     });
 };
 
-const ambiguousTimezones = /\b(PT|ET|CT|MT|CET)\b/gim;
-const convertAmbiguousToCurrent = (timezone: string) => {
-  const now = new Date();
-  switch (timezone) {
-    case "PT": {
-      const currentOffset = dayjs.tz(now, "America/Los_Angeles").utcOffset();
-      if (currentOffset == -480) return "PST";
-      else if (currentOffset == -420) return "PDT";
-      break;
-    }
-    case "ET": {
-      const currentOffset = dayjs.tz(now, "America/New_York").utcOffset();
-      if (currentOffset == -300) return "EST";
-      else if (currentOffset == -240) return "EDT";
-      break;
-    }
-    case "CT": {
-      const currentOffset = dayjs.tz(now, "America/Chicago").utcOffset();
-      if (currentOffset == -360) return "CST";
-      else if (currentOffset == -300) return "CDT";
-      break;
-    }
-    case "MT": {
-      const currentOffset = dayjs.tz(now, "America/Denver").utcOffset();
-      if (currentOffset == -420) return "MST";
-      else if (currentOffset == -360) return "MDT";
-      break;
-    }
-    case "CET": {
-      const currentOffset = dayjs.tz(now, "Europe/Paris").utcOffset();
-      if (currentOffset == 60) return "CET";
-      else if (currentOffset == 120) return "CEST";
-      break;
-    }
-    default:
-      return timezone;
-  }
-};
-const cleanAmbiguousTimezones = (text: string) => {
-  return text.replace(ambiguousTimezones, (match) =>
-    convertAmbiguousToCurrent(match)
-  );
-};
-
 export default class RemindersCreate extends Command {
   repeatRegex = /--repeat (\d*)/gim;
   stepRegex = /--step ([^-]*)/gim;
@@ -184,7 +140,7 @@ export default class RemindersCreate extends Command {
       )
         // possibly a linked tweet or other social media post, use that instead
         (parsed = strict.parse(
-          cleanAmbiguousTimezones(clickedMessage.embeds[0].description),
+          clickedMessage.embeds[0].description,
           {
             instant: clickedMessage.createdAt,
             timezone: dayjs
