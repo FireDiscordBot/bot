77fd46cc9fb089c24f6ddad576ed283e525b7636
Jake Ward <geek@gaminggeek.dev>
2024-06-21 00:17:36 +0100

    Dump commit hash to file

--- lib/util/gitUtils.ts
+++ lib/util/gitUtils.ts
@@ -2,17 +2,22 @@ import { exec } from "child_process";
 import { readFileSync } from "fs";
 
 export const getCommitHash = () => {
-  const rev = readFileSync(".git/HEAD")
-    .toString()
-    .trim()
-    .split(/.*[: ]/)
-    .slice(-1)[0];
-  if (rev.indexOf("/") == -1) {
-    return rev;
-  } else {
-    return readFileSync(".git/" + rev)
+  try {
+    return readFileSync("commit.txt").toString().trim();
+  } catch (e) {
+    // can be incorrect if we're not on the latest commit
+    // (e.g. if a deploy was reverted temporarily)
+    // but at least we'll have something
+    const rev = readFileSync(".git/HEAD")
       .toString()
-      .trim();
+      .trim()
+      .split(/.*[: ]/)
+      .slice(-1)[0];
+    if (rev.indexOf("/") == -1) return rev;
+    else
+      return readFileSync(".git/" + rev)
+        .toString()
+        .trim();
   }
 };
 
