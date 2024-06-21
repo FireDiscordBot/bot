768858b8336db6ef95f7f78cb99f103f3676bd1c
Jake Ward <geek@gaminggeek.dev>
2024-06-21 18:12:46 +0100

    Move channel create log

--- src/listeners/channelCreate.ts
+++ src/listeners/channelCreate.ts
@@ -20,10 +20,8 @@ export default class ChannelCreate extends Listener {
 
   async exec(channel: GuildBasedChannel | DMChannel) {
     if (channel instanceof DMChannel) return;
-    const guild = channel.guild as FireGuild,
-      language = guild.language;
+    const guild = channel.guild as FireGuild;
     const muteRole = guild.muteRole;
-    let muteFail = false;
     const muteCommand = this.client.getCommand("mute");
     if (
       channel instanceof GuildChannel &&
@@ -48,7 +46,7 @@ export default class ChannelCreate extends Listener {
             type: 0,
           }
         )
-        .catch(() => (muteFail = true));
+        .catch(() => {});
 
     if (!guild.permRoles) await guild.loadPermRoles();
     if (guild.permRoles.size) {
@@ -80,85 +78,6 @@ export default class ChannelCreate extends Listener {
           )
           .catch(() => {});
       }
-    } else if (!guild.permRoles) await guild.loadPermRoles();
-
-    if (guild.settings.has("log.action")) {
-      const embed = new MessageEmbed()
-        .setColor("#2ECC71")
-        .setTimestamp(channel.createdAt)
-        .setAuthor({
-          name: language.get("CHANNELCREATELOG_AUTHOR", {
-            type: titleCase(channel.type.replace(/_/g, " ")),
-            guild: guild.name,
-          }),
-          iconURL: guild.iconURL({ size: 2048, format: "png", dynamic: true }),
-        })
-        .addField(language.get("NAME"), channel.name);
-      if (channel instanceof FireTextChannel && channel.topic)
-        embed.addField(language.get("TOPIC"), channel.topic);
-      // TODO: re-add without humanize
-      // if (channel instanceof FireTextChannel && channel.rateLimitPerUser)
-      //   embed.addField(
-      //     language.get("SLOWMODE"),
-      //     humanize(channel.rateLimitPerUser, language.id.split("-")[0])
-      //   );
-      if (muteFail)
-        embed.addField(
-          language.get("WARNING"),
-          language.get("CHANNELCREATELOG_MUTE_PERMS_FAIL")
-        );
-      if (!channel.isThread() && channel.permissionOverwrites.cache.size > 1) {
-        const canView = channel.permissionOverwrites.cache
-          .filter((overwrite) =>
-            overwrite.allow.has(PermissionFlagsBits.ViewChannel)
-          )
-          .map((overwrite) => overwrite.id);
-        const roles = [
-          ...canView
-            .map((id) => guild.roles.cache.get(id))
-            .filter((role) => !!role),
-          ...guild.roles.cache
-            .filter(
-              (role) =>
-                role.permissions.has(PermissionFlagsBits.Administrator) &&
-                !canView.find((id) => id == role.id)
-            )
-            .values(),
-        ];
-        const memberIds = canView.filter(
-          (id) => !roles.find((role) => role.id == id)
-        );
-        // owner can always see
-        memberIds.push(guild.ownerId);
-        const members: string[] = memberIds.length
-          ? await guild.members
-              .fetch({ user: memberIds })
-              .then((found) => found.map((member) => member.toString()))
-              .catch(() => [])
-          : [];
-        const viewers = [...roles.map((role) => role.toString()), ...members];
-        embed.addField(language.get("VIEWABLE_BY"), `${viewers.join(" - ")}`);
-      }
-      if (guild.members.me.permissions.has(PermissionFlagsBits.ViewAuditLog)) {
-        const auditLogActions = await guild
-          .fetchAuditLogs({ limit: 2, type: "CHANNEL_CREATE" })
-          .catch(() => {});
-        if (auditLogActions) {
-          const action = auditLogActions.entries.find(
-            (entry) =>
-              // @ts-ignore
-              entry.targetType == "CHANNEL" && entry.target?.id == channel.id
-          );
-          if (action)
-            embed.addField(
-              language.get("CREATED_BY"),
-              `${action.executor} (${action.executor.id})`
-            );
-        }
-      }
-      await guild
-        .actionLog(embed, ActionLogTypes.CHANNEL_CREATE)
-        .catch(() => {});
     }
   }
 }
