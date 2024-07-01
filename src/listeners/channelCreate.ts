d2f394ae34cf6e705c6a81dfc375cfa377ad19f4
Jake Ward <geek@gaminggeek.dev>
2024-07-01 19:57:48 +0100

    Set overwrites in bulk

--- src/listeners/channelCreate.ts
+++ src/listeners/channelCreate.ts
@@ -1,13 +1,11 @@
 import { FireGuild } from "@fire/lib/extensions/guild";
-import { FireTextChannel } from "@fire/lib/extensions/textchannel";
-import { ActionLogTypes, titleCase } from "@fire/lib/util/constants";
 import { Listener } from "@fire/lib/util/listener";
 import { PermissionFlagsBits } from "discord-api-types/v9";
 import {
   DMChannel,
   GuildBasedChannel,
   GuildChannel,
-  MessageEmbed,
+  OverwriteType,
 } from "discord.js";
 
 export default class ChannelCreate extends Listener {
@@ -49,36 +47,32 @@ export default class ChannelCreate extends Listener {
         )
         .catch(() => {});
 
+    if (
+      !(channel instanceof GuildChannel) ||
+      !channel.permissionsFor(guild.me).has(PermissionFlagsBits.ManageRoles)
+    )
+      return;
+
     if (!guild.permRoles) await guild.loadPermRoles();
-    if (guild.permRoles.size) {
-      for (const [role, perms] of guild.permRoles) {
-        if (
-          !channel
-            .permissionsFor(guild.me)
-            .has(PermissionFlagsBits.ManageRoles) ||
-          !(channel instanceof GuildChannel)
+    if (guild.permRoles.size)
+      await channel.permissionOverwrites
+        .set(
+          [
+            ...channel.permissionOverwrites.cache
+              .filter(
+                // ensure the overwrites below are used instead
+                (overwrite) => !guild.permRoles.has(overwrite.id)
+              )
+              .toJSON(),
+            ...guild.permRoles.map((data, role) => ({
+              allow: data.allow,
+              deny: data.deny,
+              id: role,
+              type: "role" as OverwriteType, // idk why this is necessary but whatever
+            })),
+          ],
+          guild.language.get("PERMROLES_REASON")
         )
-          continue;
-        await channel.permissionOverwrites
-          .set(
-            [
-              ...channel.permissionOverwrites.cache
-                .filter(
-                  // ensure the overwrites below are used instead
-                  (overwrite) => overwrite.id != role
-                )
-                .toJSON(),
-              {
-                allow: perms.allow,
-                deny: perms.deny,
-                id: role,
-                type: "role",
-              },
-            ],
-            guild.language.get("PERMROLES_REASON")
-          )
-          .catch(() => {});
-      }
-    }
+        .catch(() => {});
   }
 }
