ef98fd12b3802a25fdfdbb44ede453096b42c39d
Jake Ward <geek@gaminggeek.dev>
2024-08-13 15:25:04 +0100

    Fix invite command not showing content in slash commands

--- src/commands/Main/invite.ts
+++ src/commands/Main/invite.ts
@@ -1,5 +1,6 @@
 import { ApplicationCommandMessage } from "@fire/lib/extensions/appcommandmessage";
 import { Command } from "@fire/lib/util/command";
+import { constants } from "@fire/lib/util/constants";
 import { Language } from "@fire/lib/util/language";
 
 export default class Invite extends Command {
@@ -10,15 +11,25 @@ export default class Invite extends Command {
       enableSlashCommand: true,
       restrictTo: "all",
       slashOnly: true,
+      ephemeral: true,
     });
   }
 
   async run(command: ApplicationCommandMessage) {
+    if (process.env.NODE_ENV == "development" && !command.author.isSuperuser())
+      return await command.error("INVITE_LOCAL_INSTANCE");
+
     return await command.channel.send({
-      content: `[󠄴](https://canary.discord.com/application-directory/444871677176709141  '${command.language.get(
-        "INVITE_COMMAND_APP_DIRECTORY_ALT"
-      )}')[󠄴](https://discord.gg/firebot  '${command.language.get(
-        "INVITE_COMMAND_SUPPORT_SERVER_ALT"
+      content: `${command.language.get(
+        "INVITE_RESPONSE_TEXT"
+      )}[󠄴\u200b](https://canary.discord.com/application-directory/${
+        process.env.NODE_ENV == "development"
+          ? constants.prodBotId // used for testing
+          : this.client.user.id
+      }  '${command.language.get(
+        "INVITE_APP_DIRECTORY_ALT"
+      )}')[󠄴\u200b](https://discord.gg/firebot  '${command.language.get(
+        "INVITE_SUPPORT_SERVER_ALT"
       )}')`,
     });
   }
