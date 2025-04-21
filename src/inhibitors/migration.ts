1852b687b572b0133bc3d2abcf74ccd051d9129d
Jake Ward <geek@gaminggeek.dev>
2025-04-21 17:02:52 +0100

    Bypass admin/owner commands

--- src/inhibitors/migration.ts
+++ src/inhibitors/migration.ts
@@ -1,4 +1,5 @@
 import { FireMessage } from "@fire/lib/extensions/message";
+import { Command } from "@fire/lib/util/command";
 import { Inhibitor } from "@fire/lib/util/inhibitor";
 
 export default class Migration extends Inhibitor {
@@ -9,7 +10,13 @@ export default class Migration extends Inhibitor {
     });
   }
 
-  async exec(message: FireMessage) {
+  async exec(message: FireMessage, command: Command) {
+    // bypass for admin/owner commands so they're always available
+    // (plus they should never require migration to be complete)
+    if (command.categoryID == "Admin" || command.ownerOnly) return false;
+
+    // TODO: perhaps we should call runMigration here instead of just checking?
+
     if (message.guild?.settings.unmigrated) {
       await message.error("CONFIG_GUILD_MIGRATION").catch(() => {});
       return true;
