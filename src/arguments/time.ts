77fa1ca20253676172aed3b388734cce90d9a08e
Jake Ward <geek@gaminggeek.dev>
2026-03-15 16:21:32 +0000

    Remove classic parsing from time argument

--- src/arguments/time.ts
+++ src/arguments/time.ts
@@ -3,7 +3,6 @@ import { ComponentMessage } from "@fire/lib/extensions/componentmessage";
 import { ContextCommandMessage } from "@fire/lib/extensions/contextcommandmessage";
 import { FireMessage } from "@fire/lib/extensions/message";
 import { ModalMessage } from "@fire/lib/extensions/modalmessage";
-import { classicRemind, constants } from "@fire/lib/util/constants";
 import { ParsedResult, casual } from "chrono-node";
 import * as dayjs from "dayjs";
 import { ArgumentTypeCaster } from "discord-akairo";
@@ -25,10 +24,6 @@ casual.parsers.push({
     };
   },
 });
-
-const {
-  regexes: { time },
-} = constants;
 
 export type ParsedTime = { text: string; date: Date };
 
@@ -60,36 +55,26 @@ export const parseTime = (
   forwardDate: boolean = true
 ) => {
   text = text.trim();
-  let useClassic = false;
-  for (const regex of Object.values(time)) {
-    if (Array.isArray(regex)) continue;
-    if (regex.test(text))
-      useClassic = context.author.hasExperiment(2760158308, 1);
-    regex.lastIndex = 0;
-  }
   let parsed: ParsedResult[];
-  if (useClassic)
-    parsed = classicRemind.parse(text, { instant }, { forwardDate });
-  else {
-    // Get the date first, doesn't need to be exact with timing since we only want it to get the offset
-    // This will probably break if you try to set a reminder around the switch to/from DST
-    // but if you're doing that, fuck you.
-    // timezones suck, daylight savings sucks more
-    const preliminaryParse = casual.parse(text, { instant }, { forwardDate });
-    const date = preliminaryParse[0]?.start.date();
-    if (!date) return null;
-    let offset: number;
-    if (preliminaryParse[0].start.get("timezoneOffset") == null) {
-      // Instead of the old offset we got from browsers, we'll use an IANA timezone name
-      // and that + the date from above allows us to get the correct offset for DST
-      date.setHours(23, 59, 59, 999); // should be past the dst switch in most timezones
-      offset = dayjs.tz(date, IANA).utcOffset();
-    }
-    // This means a timezone was specified in the text so we'll use that
-    else offset = preliminaryParse[0].start.get("timezoneOffset");
-
-    parsed = casual.parse(text, { instant, timezone: offset }, { forwardDate });
+  // Get the date first, doesn't need to be exact with timing since we only want it to get the offset
+  // This will probably break if you try to set a reminder around the switch to/from DST
+  // but if you're doing that, fuck you.
+  // timezones suck, daylight savings sucks more
+  const preliminaryParse = casual.parse(text, { instant }, { forwardDate });
+  const date = preliminaryParse[0]?.start.date();
+  if (!date) return null;
+  let offset: number;
+  if (preliminaryParse[0].start.get("timezoneOffset") == null) {
+    // Instead of the old offset we got from browsers, we'll use an IANA timezone name
+    // and that + the date from above allows us to get the correct offset for DST
+    date.setHours(23, 59, 59, 999); // should be past the dst switch in most timezones
+    offset = dayjs.tz(date, IANA).utcOffset();
   }
+  // This means a timezone was specified in the text so we'll use that
+  else offset = preliminaryParse[0].start.get("timezoneOffset");
+
+  parsed = casual.parse(text, { instant, timezone: offset }, { forwardDate });
+
   if (!parsed.length) return null;
 
   // idk why but "they" results in a match for a year in the future
